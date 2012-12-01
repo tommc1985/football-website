b4b7c202067cbe3691cc93b9c5d91288e55911e7
Thomas McGregor <leegleeders@yahoo.co.uk>
2012-12-01 20:59:43 +0000

    Added Cache Model

---
+++ application/models/cache_model.php
@@ -0,0 +1,70 @@
+<?php
+class Cache_model extends CI_Model {
+
+    /**
+     * CodeIgniter instance
+     * @var object
+     */
+    public $ci;
+
+    public $cacheModels;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        // Call the Model constructor
+        parent::__construct();
+
+        $this->ci =& get_instance();
+
+        $this->cacheModels = array(
+            'Cache_Club_model',
+            'Cache_Fantasy_Football_model',
+            'Cache_Player_Accumulated_model',
+            'Cache_Player_Goals_model',
+            'Cache_Player_model',
+        );
+
+        $this->loadModels();
+    }
+
+    /**
+     * Load Models
+     * @return NULL
+     */
+    public function loadModels()
+    {
+        foreach ($this->cacheModels as $modelName) {
+            $this->ci->load->model($modelName);
+        }
+    }
+
+    /**
+     * Insert rows from Cache Models into process queue table to be processed
+     * @param  int|NULL $season     Season "career"
+     * @return boolean
+     */
+    public function insertEntries($season = NULL)
+    {
+        foreach ($this->cacheModels as $modelName) {
+            $this->$modelName->insertEntries($season);
+        }
+    }
+
+    /**
+     * Process any rows that are in the cache process queues
+     * @return boolean
+     */
+    public function processQueuedRows()
+    {
+        foreach ($this->cacheModels as $modelName) {
+            $processedRows = $this->ci->$modelName->processQueuedRows();
+
+            if ($processedRows > 0) {
+                break;
+            }
+        }
+    }
+}
