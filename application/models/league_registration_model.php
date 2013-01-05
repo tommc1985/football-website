f8ddc320cd5466b2021539d5c99ac87245866944
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-05 20:39:14 +0000

    Added "fetchByLeagueId" and "fetchForDropdown" methods to League_Registration_model

--- application/models/league_registration_model.php
+++ application/models/league_registration_model.php
@@ -16,6 +16,8 @@ class League_Registration_model extends Base_Model {
         parent::__construct();
 
         $this->tableName = 'league_registration';
+
+        $this->ci->load->model('Opposition_model');
     }
 
     /**
@@ -28,6 +30,46 @@ class League_Registration_model extends Base_Model {
 
         $this->ci->form_validation->set_rules('league_id', 'League', 'trim|xss_clean');
         $this->ci->form_validation->set_rules('opposition_id', 'Team', 'trim|xss_clean');
+    }
+
+    /**
+     * Fetch all matches from a particular League
+     * @param  int $leagueId         League ID
+     * @param  int|false $limit      Number of rows to return
+     * @param  int|false $offset     The offset
+     * @param  string|false $orderBy Which fields to order results by
+     * @param  string|false $order   Order the results Ascending or Descending
+     * @return array                 Returned rows
+     */
+    public function fetchByLeagueId($leagueId)
+    {
+        $this->db->select("{$this->ci->Opposition_model->tableName}.*");
+        $this->db->from($this->tableName);
+        $this->db->join($this->ci->Opposition_model->tableName, "{$this->tableName}.opposition_id = {$this->ci->Opposition_model->tableName}.id");
+        $this->db->where('league_id', $leagueId);
+        $this->db->where("{$this->tableName}.deleted", 0);
+        $this->db->where("{$this->ci->Opposition_model->tableName}.deleted", 0);
+        $this->db->order_by("{$this->ci->Opposition_model->tableName}.name", "asc");
+
+        return $this->db->get()->result();
+    }
+
+    /**
+     * Fetch All League Registrations and format for dropdown
+     * @param  int $leagueId         League ID
+     * @return array List of League Registrations
+     */
+    public function fetchForDropdown($leagueId)
+    {
+        $results = $this->fetchByLeagueId($leagueId);
+
+        $dropdownOptions = array();
+
+        foreach ($results as $result) {
+            $dropdownOptions[$result->id] = $result->name;
+        }
+
+        return $dropdownOptions;
     }
 
 }
