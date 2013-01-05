e2f6f498577d1fca32369477dd3b5a90bfef72fe
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-05 20:55:15 +0000

    Moved "fetchForDropdown" method to bottom of Opposition_model

--- application/models/opposition_model.php
+++ application/models/opposition_model.php
@@ -19,23 +19,6 @@ class Opposition_model extends Base_Model {
     }
 
     /**
-     * Fetch All Opposition and format for dropdown
-     * @return array List of oppositions
-     */
-    public function fetchForDropdown()
-    {
-        $results = $this->fetchAll(false, false, 'name', 'asc');
-
-        $dropdownOptions = array();
-
-        foreach ($results as $result) {
-            $dropdownOptions[$result->id] = $result->name;
-        }
-
-        return $dropdownOptions;
-    }
-
-    /**
      * Return string of fields to order a SQL statement by (dependent upon argument passed)
      * @param  string $orderBy Field Name
      * @return string          Field Names
@@ -54,6 +37,23 @@ class Opposition_model extends Base_Model {
         $this->ci->load->library('form_validation');
 
         $this->ci->form_validation->set_rules('name', 'Name', 'trim|required|xss_clean');
+    }
+
+    /**
+     * Fetch All Oppositions and format for dropdown
+     * @return array List of Oppositions
+     */
+    public function fetchForDropdown()
+    {
+        $results = $this->fetchAll();
+
+        $dropdownOptions = array();
+
+        foreach ($results as $result) {
+            $dropdownOptions[$result->id] = $result->name;
+        }
+
+        return $dropdownOptions;
     }
 
 }
