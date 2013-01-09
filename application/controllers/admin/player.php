1348f08e3bda23c73013608dae09a49a36888977
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-09 18:23:33 +0000

    Corrected ordering on list page for Player

--- application/controllers/admin/player.php
+++ application/controllers/admin/player.php
@@ -39,7 +39,17 @@ class Player extends CI_Controller/*Backend_Controller*/
 
         $data['players'] = $this->Player_model->fetchAll($perPage, $offset, $parameters['order-by'], $parameters['order']);
 
-        $config['base_url'] = '/admin/player/index/offset/';
+        $config['base_url'] = '/admin/player/index/';
+
+        if ($parameters['order-by']) {
+            $config['base_url'] .= "order-by/{$parameters['order-by']}/";
+        }
+
+        if ($parameters['order']) {
+            $config['base_url'] .= "order/{$parameters['order']}/";
+        }
+
+        $config['base_url'] .= 'offset/';
         $config['total_rows'] = $this->Player_model->countAll();
         $config['per_page'] = $perPage;
         $config['cur_page'] = $offset;
