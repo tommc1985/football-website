70d221e5f90b82fcc7578c83bc008de86c4ff540
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-09 18:27:20 +0000

    Corrected ordering on list page for Competition Stage

--- application/controllers/admin/competition_stage.php
+++ application/controllers/admin/competition_stage.php
@@ -40,6 +40,16 @@ class Competition_Stage extends CI_Controller/*Backend_Controller*/
         $data['competitionStages'] = $this->Competition_Stage_model->fetchAll($perPage, $offset, $parameters['order-by'], $parameters['order']);
 
         $config['base_url'] = '/admin/competition-stage/index/offset/';
+
+        if ($parameters['order-by']) {
+            $config['base_url'] .= "order-by/{$parameters['order-by']}/";
+        }
+
+        if ($parameters['order']) {
+            $config['base_url'] .= "order/{$parameters['order']}/";
+        }
+
+        $config['base_url'] .= 'offset/';
         $config['total_rows'] = $this->Competition_Stage_model->countAll();
         $config['per_page'] = $perPage;
         $config['cur_page'] = $offset;
