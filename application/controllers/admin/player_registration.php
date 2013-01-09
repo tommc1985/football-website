5cf5a5d9e9509a91ad6df527febc163a5c36f873
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-09 18:32:34 +0000

    Corrected ordering on list page for Player Registration

--- application/controllers/admin/player_registration.php
+++ application/controllers/admin/player_registration.php
@@ -42,6 +42,16 @@ class Player_Registration extends CI_Controller/*Backend_Controller*/
         $data['playerRegistrations'] = $this->Player_Registration_model->fetchAll($perPage, $offset, $parameters['order-by'], $parameters['order']);
 
         $config['base_url'] = '/admin/player-registration/index/offset/';
+
+        if ($parameters['order-by']) {
+            $config['base_url'] .= "order-by/{$parameters['order-by']}/";
+        }
+
+        if ($parameters['order']) {
+            $config['base_url'] .= "order/{$parameters['order']}/";
+        }
+
+        $config['base_url'] .= 'offset/';
         $config['total_rows'] = $this->Player_Registration_model->countAll();
         $config['per_page'] = $perPage;
         $config['cur_page'] = $offset;
