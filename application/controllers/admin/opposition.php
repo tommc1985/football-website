fd7fddc2e49feb24374adbfaa3cdbffe14c94405
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-01-09 18:32:10 +0000

    Corrected ordering on list page for Opposition

--- application/controllers/admin/opposition.php
+++ application/controllers/admin/opposition.php
@@ -40,6 +40,16 @@ class Opposition extends CI_Controller/*Backend_Controller*/
         $data['oppositions'] = $this->Opposition_model->fetchAll($perPage, $offset, $parameters['order-by'], $parameters['order']);
 
         $config['base_url'] = '/admin/opposition/index/offset/';
+
+        if ($parameters['order-by']) {
+            $config['base_url'] .= "order-by/{$parameters['order-by']}/";
+        }
+
+        if ($parameters['order']) {
+            $config['base_url'] .= "order/{$parameters['order']}/";
+        }
+
+        $config['base_url'] .= 'offset/';
         $config['total_rows'] = $this->Opposition_model->countAll();
         $config['per_page'] = $perPage;
         $config['cur_page'] = $offset;
