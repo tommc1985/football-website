205dda937761f3a34796ab2bd5b7bf6f880c2631
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-02-16 20:25:29 +0000

    First attempt at Appearances form

--- application/views/admin/appearance/form.php
+++ application/views/admin/appearance/form.php
@@ -1,195 +1,118 @@
 <?php
-$id = array(
-    'name'  => 'id',
-    'id'    => 'id',
-    'value' => set_value('id'),
+$match_id = array(
+    'name'  => 'match_id',
+    'id'    => 'match_id',
+    'value' => set_value('match_id'),
 );
 
-$opposition_id = array(
-    'name'    => 'opposition_id',
-    'id'      => 'opposition_id',
-    'options' => array('' => '--- Select ---') + $this->Opposition_model->fetchForDropdown(),
-    'value'   => set_value('opposition_id'),
-);
+foreach ($playerCounts as $appearanceType => $playerCount) {
+    $i = 0;
+    while($i < $playerCount) {
+        $id[$appearanceType][$i] = array(
+            'name'  => "id[{$appearanceType}][{$i}]",
+            'id'    => "id_{$appearanceType}_{$i}",
+            'value' => set_value("id[{$appearanceType}][{$i}]"),
+        );
 
-$competition_id = array(
-    'name'    => 'competition_id',
-    'id'      => 'competition_id',
-    'options' => array('' => '--- Select ---') + $this->Competition_model->fetchForDropdown(),
-    'value'   => set_value('competition_id'),
-);
+        $player_id[$appearanceType][$i] = array(
+            'name'    => "player_id[{$appearanceType}][{$i}]",
+            'id'      => "player_id_{$appearanceType}_{$i}",
+            'options' => array('' => '--- Select ---') + $this->Player_model->fetchForDropdown(),
+            'value'   => set_value("player_id[{$appearanceType}][{$i}]"),
+        );
 
-$competition_stage_id = array(
-    'name'    => 'competition_stage_id',
-    'id'      => 'competition_stage_id',
-    'options' => array('' => '--- Select ---') + $this->Competition_Stage_model->fetchForDropdown(),
-    'value'   => set_value('competition_stage_id'),
-);
+        $captain[$appearanceType][$i] = array(
+            'name'    => "captain",
+            'id'      => "captain_{$appearanceType}_{$i}",
+            'value'   => $i,
+        );
 
-$venue = array(
-    'name'    => 'venue',
-    'id'      => 'venue',
-    'options' => array('' => '--- Select ---') + $this->Match_model->fetchVenues(),
-    'value'   => set_value('venue'),
-);
+        $rating[$appearanceType][$i] = array(
+            'name'  => "rating[{$appearanceType}][{$i}]",
+            'id'    => "rating_{$appearanceType}_{$i}",
+            'value' => set_value("rating[{$appearanceType}][{$i}]"),
+        );
 
-$location = array(
-    'name'  => 'location',
-    'id'    => 'location',
-    'value' => set_value('location'),
-);
+        $motm[$appearanceType][$i] = array(
+            'name'    => "motm",
+            'id'      => "motm_{$appearanceType}_{$i}",
+            'value'   => set_value("motm[{$appearanceType}][{$i}]"),
+        );
 
-$official_id = array(
-    'name'    => 'official_id',
-    'id'      => 'official_id',
-    'options' => array('' => '--- Select ---') + $this->Official_model->fetchForDropdown(),
-    'value'   => set_value('official_id'),
-);
+        $injury[$appearanceType][$i] = array(
+            'name'  => "injury[{$appearanceType}][{$i}]",
+            'id'    => "injury_{$appearanceType}_{$i}",
+            'value' => $i,
+        );
 
-$h = array(
-    'name'  => 'h',
-    'id'    => 'h',
-    'value' => set_value('h'),
-);
+        $position[$appearanceType][$i] = array(
+            'name'  => "position[{$appearanceType}][{$i}]",
+            'id'    => "position_{$appearanceType}_{$i}",
+            'options' => array('' => '--- Select ---') + $this->Position_model->fetchForDropdown(),
+            'value' => set_value("position[{$appearanceType}][{$i}]"),
+        );
 
-$a = array(
-    'name'  => 'a',
-    'id'    => 'a',
-    'value' => set_value('a'),
-);
+        $order[$appearanceType][$i] = array(
+            'name'  => "order[{$appearanceType}][{$i}]",
+            'id'    => "order_{$appearanceType}_{$i}",
+            'value' => ($i + 1),
+        );
 
-$report = array(
-    'name'  => 'report',
-    'id'    => 'report',
-    'value' => set_value('report'),
-);
+        $shirt[$appearanceType][$i] = array(
+            'name'  => "shirt[{$appearanceType}][{$i}]",
+            'id'    => "shirt_{$appearanceType}_{$i}",
+            'value' => set_value("shirt[{$appearanceType}][{$i}]"),
+        );
 
-$date = array(
-    'name'  => 'date',
-    'id'    => 'date',
-    'value' => set_value('date'),
-);
+        $on[$appearanceType][$i] = array(
+            'name'  => "on[{$appearanceType}][{$i}]",
+            'id'    => "on_{$appearanceType}_{$i}",
+            'value' => set_value("on[{$appearanceType}][{$i}]"),
+        );
 
-$time = array(
-    'name'  => 'time',
-    'id'    => 'time',
-    'value' => set_value('time'),
-);
+        $off[$appearanceType][$i] = array(
+            'name'  => "off[{$appearanceType}][{$i}]",
+            'id'    => "off_{$appearanceType}_{$i}",
+            'value' => set_value("off[{$appearanceType}][{$i}]"),
+        );
 
-$h_et = array(
-    'name'  => 'h_et',
-    'id'    => 'h_et',
-    'value' => set_value('h_et'),
-);
-
-$a_et = array(
-    'name'  => 'a_et',
-    'id'    => 'a_et',
-    'value' => set_value('a_et'),
-);
-
-$h_pen = array(
-    'name'  => 'h_pen',
-    'id'    => 'h_pen',
-    'value' => set_value('h_pen'),
-);
-
-$a_pen = array(
-    'name'  => 'a_pen',
-    'id'    => 'a_pen',
-    'value' => set_value('a_pen'),
-);
-
-$status = array(
-    'name'    => 'status',
-    'id'      => 'status',
-    'options' => array('' => '--- Select ---') + $this->Match_model->fetchStatuses(),
-    'value'   => set_value('status'),
-);
+        $i++;
+    }
+}
 
 echo form_open($this->uri->uri_string()); ?>
 <table>
-    <?php echo form_hidden('id', set_value('id', isset($match->id) ? $match->id : '')); ?>
+    <?php echo form_hidden('match_id', set_value('match_id', isset($match->id) ? $match->id : '')); ?>
     <tr>
-        <td><?php echo form_label('Date', $date['name']); ?></td>
-        <td><?php echo form_date($date['name'], set_value($date['name'], isset($match->date) ? substr($match->date, 0, 10) : '')); ?></td>
-        <td class="error"><?php echo form_error($date['name']); ?><?php echo isset($errors[$date['name']]) ? $errors[$date['name']] : ''; ?></td>
+        <td><?php echo form_label('Player', ''); ?></td>
+        <td><?php echo form_label('Captain', ''); ?></td>
+        <td><?php echo form_label('Rating', ''); ?></td>
+        <td><?php echo form_label('MOTM', ''); ?></td>
+        <td><?php echo form_label('Injury', ''); ?></td>
+        <td><?php echo form_label('Position', ''); ?></td>
+        <td><?php echo form_label('Shirt', ''); ?></td>
+        <td><?php echo form_label('On', ''); ?></td>
+        <td><?php echo form_label('Off', ''); ?></td>
     </tr>
+    <?php
+    foreach ($playerCounts as $appearanceType => $playerCount) {
+        $i = 0;
+        while($i < $playerCount) { ?>
     <tr>
-        <td><?php echo form_label('Time', $time['name']); ?></td>
-        <td><?php echo form_time($time['name'], set_value($time['name'], isset($match->date) ? substr($match->date, 11, 5) : Configuration::get('usual_match_ko_time'))); ?></td>
-        <td class="error"><?php echo form_error($time['name']); ?><?php echo isset($errors[$time['name']]) ? $errors[$time['name']] : ''; ?></td>
+        <td><?php echo form_dropdown($player_id[$appearanceType][$i]['name'], $player_id[$appearanceType][$i]['options'], set_value($player_id[$appearanceType][$i]['value'])); ?></td>
+        <td><?php echo form_radio($captain[$appearanceType][$i]); ?></td>
+        <td><?php echo form_input($rating[$appearanceType][$i]); ?></td>
+        <td><?php echo form_radio($motm[$appearanceType][$i]); ?></td>
+        <td><?php echo form_checkbox($injury[$appearanceType][$i]); ?></td>
+        <td><?php echo form_dropdown($position[$appearanceType][$i]['name'], $position[$appearanceType][$i]['options'], set_value($position[$appearanceType][$i]['value'])); ?></td>
+        <td><?php echo form_input($shirt[$appearanceType][$i]); ?></td>
+        <td><?php echo form_input($on[$appearanceType][$i]); ?></td>
+        <td><?php echo form_input($off[$appearanceType][$i]); ?></td>
     </tr>
-    <tr>
-        <td><?php echo form_label('Opposition', $opposition_id['name']); ?></td>
-        <td><?php echo form_dropdown($opposition_id['name'], $opposition_id['options'], set_value($opposition_id['name'], isset($match->opposition_id) ? $match->opposition_id : '')); ?></td>
-        <td class="error"><?php echo form_error($opposition_id['name']); ?><?php echo isset($errors[$opposition_id['name']]) ? $errors[$opposition_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Competition', $competition_id['name']); ?></td>
-        <td><?php echo form_dropdown($competition_id['name'], $competition_id['options'], set_value($competition_id['name'], isset($match->competition_id) ? $match->competition_id : '')); ?></td>
-        <td class="error"><?php echo form_error($competition_id['name']); ?><?php echo isset($errors[$competition_id['name']]) ? $errors[$competition_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Stage', $competition_stage_id['name']); ?></td>
-        <td><?php echo form_dropdown($competition_stage_id['name'], $competition_stage_id['options'], set_value($competition_stage_id['name'], isset($match->competition_stage_id) ? $match->competition_stage_id : '')); ?></td>
-        <td class="error"><?php echo form_error($competition_stage_id['name']); ?><?php echo isset($errors[$competition_stage_id['name']]) ? $errors[$competition_stage_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Venue', $venue['name']); ?></td>
-        <td><?php echo form_dropdown($venue['name'], $venue['options'], set_value($venue['name'], isset($match->venue) ? $match->venue : '')); ?></td>
-        <td class="error"><?php echo form_error($venue['name']); ?><?php echo isset($errors[$venue['name']]) ? $errors[$venue['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Location', $location['name']); ?></td>
-        <td><?php echo form_input($location['name'], set_value($location['name'], isset($match->location) ? $match->location : '')); ?></td>
-        <td class="error"><?php echo form_error($location['name']); ?><?php echo isset($errors[$location['name']]) ? $errors[$location['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Official', $official_id['name']); ?></td>
-        <td><?php echo form_dropdown($official_id['name'], $official_id['options'], set_value($official_id['name'], isset($match->official_id) ? $match->official_id : '')); ?></td>
-        <td class="error"><?php echo form_error($official_id['name']); ?><?php echo isset($errors[$official_id['name']]) ? $errors[$official_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Your Score', $h['name']); ?></td>
-        <td><?php echo form_input($h['name'], set_value($h['name'], isset($match->h) ? $match->h : '')); ?></td>
-        <td class="error"><?php echo form_error($h['name']); ?><?php echo isset($errors[$h['name']]) ? $errors[$h['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Opposition Score', $a['name']); ?></td>
-        <td><?php echo form_input($a['name'], set_value($a['name'], isset($match->a) ? $match->a : '')); ?></td>
-        <td class="error"><?php echo form_error($a['name']); ?><?php echo isset($errors[$a['name']]) ? $errors[$a['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Report', $report['name']); ?></td>
-        <td><?php echo form_textarea($report['name'], set_value($report['name'], isset($match->report) ? $match->report : '')); ?></td>
-        <td class="error"><?php echo form_error($report['name']); ?><?php echo isset($errors[$report['name']]) ? $errors[$report['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Your Score', $h_et['name']); ?></td>
-        <td><?php echo form_input($h_et['name'], set_value($h_et['name'], isset($match->h_et) ? $match->h_et : '')); ?></td>
-        <td class="error"><?php echo form_error($h_et['name']); ?><?php echo isset($errors[$h_et['name']]) ? $errors[$h_et['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Opposition Score', $a_et['name']); ?></td>
-        <td><?php echo form_input($a_et['name'], set_value($a_et['name'], isset($match->a_et) ? $match->a_et : '')); ?></td>
-        <td class="error"><?php echo form_error($a_et['name']); ?><?php echo isset($errors[$a_et['name']]) ? $errors[$a_et['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Your Penalty Score', $h_pen['name']); ?></td>
-        <td><?php echo form_input($h_pen['name'], set_value($h_pen['name'], isset($match->h_pen) ? $match->h_pen : '')); ?></td>
-        <td class="error"><?php echo form_error($h_pen['name']); ?><?php echo isset($errors[$h_pen['name']]) ? $errors[$h_pen['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Opposition Penalty Score', $a_pen['name']); ?></td>
-        <td><?php echo form_input($a_pen['name'], set_value($a_pen['name'], isset($match->a_pen) ? $match->a_pen : '')); ?></td>
-        <td class="error"><?php echo form_error($a_pen['name']); ?><?php echo isset($errors[$a_pen['name']]) ? $errors[$a_pen['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label('Status', $status['name']); ?></td>
-        <td><?php echo form_dropdown($status['name'], $status['options'], set_value($status['name'], isset($match->status) ? $match->status : '')); ?></td>
-        <td class="error"><?php echo form_error($status['name']); ?><?php echo isset($errors[$status['name']]) ? $errors[$status['name']] : ''; ?></td>
-    </tr>
+    <?php
+            $i++;
+        }
+    } ?>
 </table>
 <?php echo form_submit('submit', $submitButtonText); ?>
 <?php echo form_close(); ?>
