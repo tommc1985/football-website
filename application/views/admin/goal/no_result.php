88c0ff541e316eeace920c5e4f7f87f92023fe8f
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-03-02 21:22:43 +0000

    Correct text in Goal not found/no result views

--- application/views/admin/goal/no_result.php
+++ application/views/admin/goal/no_result.php
@@ -1,7 +1,7 @@
 <?php
 $this->load->view('admin/header'); ?>
 
-<p>Cannot edit appearance details until the match result is entered.</p>
+<p>Cannot edit goal details until the match result is entered.</p>
 
 <?php
 $this->load->view('admin/footer');
