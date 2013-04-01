e01fc2616f0013d0243e04d0fcb4797fd6447966
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-01 18:02:48 +0100

    Added Competition Stage Helper

---
+++ application/helpers/competition_stage_helper.php
@@ -0,0 +1,59 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Competition Stage Helper
+ */
+class Competition_stage_helper
+{
+    /**
+     * Constructor
+     */
+    public function __construct() { }
+
+    protected static function _fetchObject($id)
+    {
+        $ci =& get_instance();
+
+        $ci->load->database();
+        $ci->load->model('Competition_Stage_model');
+
+        return $ci->Competition_Stage_model->fetch($id);
+    }
+
+    protected static function _convertObject($object)
+    {
+        if (!is_object($object)) {
+            if (is_array($object)) {
+                $object = (object) $object;
+            } else {
+                $object = self::_fetchObject($object);
+            }
+        }
+
+        return $object;
+    }
+
+    /**
+     * Return a Competition Stage's Name
+     * @param  mixed $competitionStage  Competition Stage Object/Array
+     * @return string                   The Competition Stage's Name
+     */
+    public static function name($competitionStage)
+    {
+        $competitionStage = self::_convertObject($competitionStage);
+
+        return $competitionStage->name;
+    }
+
+    /**
+     * Return a Competition Stage's Abbreviation
+     * @param  mixed $competitionStage  Competition Stage Object/Array
+     * @return string                   The Competition Stage's Abbreviation
+     */
+    public static function abbreviation($competitionStage)
+    {
+        $competitionStage = self::_convertObject($competitionStage);
+
+        return $competitionStage->abbreviation;
+    }
+}
