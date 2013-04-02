3fdbfaf09fff8905805cddb50a585df91944ba23
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-02 18:40:27 +0100

    Method to create "opposition" table in Install Model

--- application/models/install/install_model.php
+++ application/models/install/install_model.php
@@ -2045,5 +2045,48 @@ class Install_Model extends CI_Model {
         return false;
     }
 
+    /**
+     * Create 'opposition' table
+     * @param  string $tableName Database table name
+     * @return boolean           Result of table creation attempt
+     */
+    public function createOppositionTable($tableName)
+    {
+        $fields = array(
+            'id' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE,
+            ),
+            'name' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 255,
+            ),
+            'date_added' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'date_updated' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'deleted' => array(
+                'type'           => 'TINYINT',
+                'constraint'     => 1,
+                'default'        => 0,
+            ),
+        );
+
+        $this->ci->dbforge->add_field($fields);
+        $this->ci->dbforge->add_key('id', TRUE);
+
+        if ($this->ci->dbforge->create_table($tableName, TRUE)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
