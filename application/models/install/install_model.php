4ad4a308b06fe97c415462ffdd43ea0c0fae0b8b
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-02 18:46:56 +0100

    Method to create "player_registration" table in Install Model

--- application/models/install/install_model.php
+++ application/models/install/install_model.php
@@ -2165,5 +2165,53 @@ class Install_Model extends CI_Model {
         return false;
     }
 
+    /**
+     * Create player_registration' table
+     * @param  string $tableName Database table name
+     * @return boolean           Result of table creation attempt
+     */
+    public function createPlayerRegistrationTable($tableName)
+    {
+        $fields = array(
+            'id' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE,
+            ),
+            'player_id' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+            ),
+            'season' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'date_added' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'date_updated' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'deleted' => array(
+                'type'           => 'TINYINT',
+                'constraint'     => 1,
+                'default'        => 0,
+            ),
+        );
+
+        $this->ci->dbforge->add_field($fields);
+        $this->ci->dbforge->add_key('id', TRUE);
+
+        if ($this->ci->dbforge->create_table($tableName, TRUE)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
