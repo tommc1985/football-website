c0b0dad22d3d15e3678c0bc5b999a8a04b955f8d
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-02 17:50:09 +0100

    Method to create "image" table in Install Model

--- application/models/install/install_model.php
+++ application/models/install/install_model.php
@@ -1574,5 +1574,76 @@ class Install_Model extends CI_Model {
         return false;
     }
 
+    /**
+     * Create 'image' table
+     * @param  string $tableName Database table name
+     * @return boolean           Result of table creation attempt
+     */
+    public function createImageTable($tableName)
+    {
+        $fields = array(
+            'id' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => TRUE,
+                'auto_increment' => TRUE,
+            ),
+            'title' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 255,
+            ),
+            'filename' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 20,
+            ),
+            'extension' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 4,
+            ),
+            'directory' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 15,
+            ),
+            'width' => array(
+                'type'           => 'INT',
+                'constraint'     => 5,
+            ),
+            'height' => array(
+                'type'           => 'INT',
+                'constraint'     => 5,
+            ),
+            'filesize' => array(
+                'type'           => 'INT',
+                'constraint'     => 20,
+            ),
+            'comment' => array(
+                'type'           => 'VARCHAR',
+                'constraint'     => 1000,
+            ),
+            'date_added' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'date_updated' => array(
+                'type'           => 'INT',
+                'constraint'     => 11,
+            ),
+            'deleted' => array(
+                'type'           => 'TINYINT',
+                'constraint'     => 1,
+                'default'        => 0,
+            ),
+        );
+
+        $this->ci->dbforge->add_field($fields);
+        $this->ci->dbforge->add_key('id', TRUE);
+
+        if ($this->ci->dbforge->create_table($tableName, TRUE)) {
+            return true;
+        }
+
+        return false;
+    }
+
 
 }
