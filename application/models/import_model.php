3e022f4f993a88f8a566de50f85b5a8c0f69975b
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-03 17:52:12 +0100

    Import of Player Data

--- application/models/import_model.php
+++ application/models/import_model.php
@@ -239,6 +239,27 @@ class Import_Model extends CI_Model {
     }
 
     /**
+     * Insert Player Data
+     * @return NULL
+     */
+    public function importPlayerData()
+    {
+        $objects = $this->fetchAll('_player');
+
+        $data = array();
+
+        foreach ($objects as $object) {
+            $object = $object;
+            $object->date_added = time();
+            $object->date_updated = time();
+
+            $data[] = (array) $object;
+        }
+
+        $this->db->insert_batch('player', $data);
+    }
+
+    /**
      * Fetch all rows for specified table
      * @param  string $tableName     Database Table
      * @param  string $orderBy       Which fields to order results by
