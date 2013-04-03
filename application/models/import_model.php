273f4e40d80f4c38d96b306a891aea300d7c8c5f
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-03 17:21:50 +0100

    Import of Competition Stage Data

--- application/models/import_model.php
+++ application/models/import_model.php
@@ -105,6 +105,27 @@ class Import_Model extends CI_Model {
     }
 
     /**
+     * Insert Competition Stage Data
+     * @return NULL
+     */
+    public function importCompetitionStageData()
+    {
+        $objects = $this->fetchAll('_competition_stage');
+
+        $data = array();
+
+        foreach ($objects as $object) {
+            $object = $object;
+            $object->date_added = time();
+            $object->date_updated = time();
+
+            $data[] = (array) $object;
+        }
+
+        $this->db->insert_batch('competition_stage', $data);
+    }
+
+    /**
      * Fetch all rows for specified table
      * @param  string $tableName     Database Table
      * @param  string $orderBy       Which fields to order results by
