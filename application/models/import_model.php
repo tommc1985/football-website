fedc1d21e99ebfb140aeb2975034760013677bdd
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-03 17:49:21 +0100

    Import of Opposition Data

--- application/models/import_model.php
+++ application/models/import_model.php
@@ -218,6 +218,27 @@ class Import_Model extends CI_Model {
     }
 
     /**
+     * Insert Opposition Data
+     * @return NULL
+     */
+    public function importOppositionData()
+    {
+        $objects = $this->fetchAll('_opposition');
+
+        $data = array();
+
+        foreach ($objects as $object) {
+            $object = $object;
+            $object->date_added = time();
+            $object->date_updated = time();
+
+            $data[] = (array) $object;
+        }
+
+        $this->db->insert_batch('opposition', $data);
+    }
+
+    /**
      * Fetch all rows for specified table
      * @param  string $tableName     Database Table
      * @param  string $orderBy       Which fields to order results by
