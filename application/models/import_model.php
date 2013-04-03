060c83f193ed506d10f93b2df7c7bbbd9f0f2ba3
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-03 17:24:23 +0100

    Import of Content Data

--- application/models/import_model.php
+++ application/models/import_model.php
@@ -126,6 +126,29 @@ class Import_Model extends CI_Model {
     }
 
     /**
+     * Insert Content Data
+     * @return NULL
+     */
+    public function importContentData()
+    {
+        $objects = $this->fetchAll('_content');
+
+        $data = array();
+
+        foreach ($objects as $object) {
+            $object = $object;
+            unset($object->insert_date);
+            unset($object->last_updated);
+            $object->date_added = time();
+            $object->date_updated = time();
+
+            $data[] = (array) $object;
+        }
+
+        $this->db->insert_batch('content', $data);
+    }
+
+    /**
      * Fetch all rows for specified table
      * @param  string $tableName     Database Table
      * @param  string $orderBy       Which fields to order results by
