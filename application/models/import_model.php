c50a12ae24068f252dc0f65c897a345c0b66b6c0
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-03 17:48:12 +0100

    Import of Official Data

--- application/models/import_model.php
+++ application/models/import_model.php
@@ -197,6 +197,27 @@ class Import_Model extends CI_Model {
     }
 
     /**
+     * Insert Official Data
+     * @return NULL
+     */
+    public function importOfficialData()
+    {
+        $objects = $this->fetchAll('_official');
+
+        $data = array();
+
+        foreach ($objects as $object) {
+            $object = $object;
+            $object->date_added = time();
+            $object->date_updated = time();
+
+            $data[] = (array) $object;
+        }
+
+        $this->db->insert_batch('official', $data);
+    }
+
+    /**
      * Fetch all rows for specified table
      * @param  string $tableName     Database Table
      * @param  string $orderBy       Which fields to order results by
