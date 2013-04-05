234eae9b3f38d7d4ae0ade8f364a153d0cb09688
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-05 18:52:40 +0100

    Added Position Helper

---
+++ application/helpers/position_helper.php
@@ -0,0 +1,59 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Position Helper
+ */
+class Position_helper
+{
+    /**
+     * Constructor
+     */
+    public function __construct() { }
+
+    protected static function _fetchObject($id)
+    {
+        $ci =& get_instance();
+
+        $ci->load->database();
+        $ci->load->model('Position_model');
+
+        return $ci->Position_model->fetch($id);
+    }
+
+    protected static function _convertObject($object)
+    {
+        if (!is_object($object)) {
+            if (is_array($object)) {
+                $object = (object) $object;
+            } else {
+                $object = self::_fetchObject($object);
+            }
+        }
+
+        return $object;
+    }
+
+    /**
+     * Return a Position's Long Name
+     * @param  mixed $position     Position Object/Array
+     * @return string              The Position's Long Name
+     */
+    public static function name($position)
+    {
+        $position = self::_convertObject($position);
+
+        return $position->long_name;
+    }
+
+    /**
+     * Return a Position's Abbreviation
+     * @param  mixed $position     Position Object/Array
+     * @return string              The Position's Abbreviation
+     */
+    public static function abbreviation($position)
+    {
+        $position = self::_convertObject($position);
+
+        return $position->abbreviation;
+    }
+}
