d08347f8c5a7f832f5e997d0832c9695c0ff9e01
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-05 19:42:11 +0100

    Alphabetised language variables in Global Language file

--- application/language/english/global_lang.php
+++ application/language/english/global_lang.php
@@ -1,13 +1,14 @@
 <?php
 
 // Frontend Text
-$lang['global_n_a']                     = 'N/A';
-$lang['global_years']                   = 'years';
-$lang['global_year']                    = 'year';
 $lang['global_days']                    = 'days';
 $lang['global_day']                     = 'day';
 $lang['global_games']                   = 'games';
 $lang['global_game']                    = 'game';
+$lang['global_n_a']                     = 'N/A';
+$lang['global_none']                    = 'None';
+$lang['global_years']                   = 'years';
+$lang['global_year']                    = 'year';
 
 /* End of file global.php */
 /* Location: ./application/language/english/global.php */
