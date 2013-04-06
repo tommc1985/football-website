ee589abc4a60e360070dbcdb94082c2a44cbbf10
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-06 18:00:13 +0100

    Added "Unknown" to Global language file

--- application/language/english/global_lang.php
+++ application/language/english/global_lang.php
@@ -7,6 +7,7 @@ $lang['global_games']                   = 'games';
 $lang['global_game']                    = 'game';
 $lang['global_n_a']                     = 'N/A';
 $lang['global_none']                    = 'None';
+$lang['global_unknown']                 = 'Unknown';
 $lang['global_years']                   = 'years';
 $lang['global_year']                    = 'year';
 
