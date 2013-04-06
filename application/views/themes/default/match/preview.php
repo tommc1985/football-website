902968f13dae3caedc8ec698e4f8cab873a3ecb7
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-04-06 20:02:55 +0100

    Added headings to Match Preview

--- application/views/themes/default/match/preview.php
+++ application/views/themes/default/match/preview.php
@@ -1,1 +1,6 @@
 <h2><?php echo $this->lang->line('match_match_preview'); ?></h2>
+
+<h3><?php echo $this->lang->line('match_details'); ?></h3>
+
+<h3><?php echo $this->lang->line('match_milestones'); ?></h3>
+
