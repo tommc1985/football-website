da3cb07c2ed0f69d794d13e6d80e39e7842fb286
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-05-03 20:48:36 +0100

    Added pagination to Article listings page

--- application/controllers/article.php
+++ application/controllers/article.php
@@ -23,6 +23,7 @@ class Article extends Frontend_Controller {
      */
     public function index()
     {
+        $this->load->library('pagination');
         $parameters = $this->uri->uri_to_assoc(3, array('offset'));
 
         $perPage = Configuration::get('articles_per_page');
@@ -37,8 +38,20 @@ class Article extends Frontend_Controller {
             ), $perPage, $offset
         );
 
+        $config['base_url'] = '/article/index/offset/';
+        $config['total_rows'] = count($this->Content_model->fetchAll(
+            array(
+                'type' => 'article'
+            )
+        ));
+        $config['per_page'] = $perPage;
+        $config['cur_page'] = $offset;
+
+        $this->pagination->initialize($config);
+
         $data = array(
             'articles' => $articles,
+            'pagination' => $this->pagination->create_links(),
         );
 
         $this->load->view("themes/{$this->theme}/header", $data);
