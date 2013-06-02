6a5512032d2f38ba121b06410b18970f78b4acd6
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-06-02 17:51:30 +0100

    Added Responsive solution to Fixtures page

--- application/views/themes/default/match/welcome_message.php
+++ application/views/themes/default/match/welcome_message.php
@@ -1,12 +1,12 @@
 <h2><?php echo sprintf($this->lang->line('match_fixtures_and_results_for_season'), Utility_helper::formattedSeason($season)); ?></h2>
 
-<table>
+<table class="no-more-tables">
     <thead>
         <tr>
             <td><?php echo $this->lang->line('match_date'); ?></td>
             <td><?php echo $this->lang->line('match_opposition'); ?></td>
             <td><?php echo $this->lang->line('match_competition'); ?></td>
-            <td><?php echo $this->lang->line('match_h_a'); ?></td>
+            <td><?php echo $this->lang->line('match_venue'); ?></td>
             <td><?php echo $this->lang->line('match_score'); ?></td>
         </tr>
     </thead>
@@ -16,11 +16,11 @@ if ($matches) { ?>
 <?php
     foreach ($matches as $match) { ?>
         <tr>
-            <td><?php echo Utility_helper::formattedDate($match->date, "jS M 'y"); ?></td>
-            <td><?php echo Opposition_helper::name($match->opposition_id); ?></td>
-            <td><?php echo Match_helper::fullCompetitionNameCombined($match); ?></td>
-            <td><?php echo Match_helper::venue($match); ?></td>
-            <td><?php echo anchor('/match/view/id/' . $match->id, Match_helper::score($match)); ?></td>
+            <td data-title="<?php echo $this->lang->line('match_date'); ?>"><?php echo Utility_helper::formattedDate($match->date, "jS M 'y"); ?></td>
+            <td data-title="<?php echo $this->lang->line('match_opposition'); ?>"><?php echo Opposition_helper::name($match->opposition_id); ?></td>
+            <td data-title="<?php echo $this->lang->line('match_competition'); ?>"><?php echo Match_helper::fullCompetitionNameCombined($match); ?></td>
+            <td data-title="<?php echo $this->lang->line('match_venue'); ?>"><?php echo Match_helper::longVenue($match); ?></td>
+            <td data-title="<?php echo $this->lang->line('match_score'); ?>"><?php echo anchor('/match/view/id/' . $match->id, Match_helper::score($match)); ?></td>
         </tr>
 <?php
     }
