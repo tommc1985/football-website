ae46e1fd98f2c237c8a7b758b1755ddd8164a18e
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-06-05 20:29:27 +0100

    Added "Unknown" condition to Match Location

--- application/views/themes/default/match/result.php
+++ application/views/themes/default/match/result.php
@@ -6,7 +6,7 @@
 <?php echo $this->lang->line('match_score'); ?> <?php echo Match_helper::longScore($match); ?><br />
 <?php echo $this->lang->line('match_venue'); ?> <?php echo Match_helper::longVenue($match) . ' ' . $this->lang->line('match_versus') . ' ' . Opposition_helper::name($match->opposition_id); ?><br />
 <?php echo $this->lang->line('match_competition'); ?> <?php echo Match_helper::fullCompetitionNameCombined($match); ?><br />
-<?php echo $this->lang->line('match_location'); ?> <?php echo $match->location; ?><br />
+<?php echo $this->lang->line('match_location'); ?> <?php echo $match->location ? $match->location : $this->lang->line('global_unknown'); ?><br />
 <?php echo $this->lang->line('match_official'); ?> <?php echo $match->official_id == 0 ? $this->lang->line('global_unknown') : Official_helper::initialSurname($match->official_id); ?></p>
 
 <?php
