1ad36dd4f6335dd496fefce6a0b229d641f1c7f8
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-06-05 15:24:46 +0100

    Added Responsive solution to Player page

--- application/views/themes/default/player/welcome_message.php
+++ application/views/themes/default/player/welcome_message.php
@@ -1,6 +1,6 @@
 <h2><?php echo $this->lang->line('player_squad_list'); ?></h2>
 
-<table>
+<table class="no-more-tables">
     <thead>
         <tr>
             <td><?php echo $this->lang->line('player_player'); ?></td>
@@ -24,17 +24,17 @@ if ($players) { ?>
 <?php
     foreach ($players as $player) { ?>
         <tr>
-            <td><?php echo anchor('/player/view/id/' . $player->id, Player_helper::fullNameReverse($player)); ?></td>
-            <td><?php echo Utility_helper::formattedDate($player->dob, "jS M Y"); ?></td>
-            <td><?php echo $player->appearances; ?> (<?php echo $player->substitute_appearances; ?>)</td>
-            <td><?php echo $player->goals; ?></td>
-            <td><?php echo $player->assists; ?></td>
-            <td><?php echo $player->motms; ?></td>
-            <td><?php echo $player->yellows; ?></td>
-            <td><?php echo $player->reds; ?></td>
+            <td data-title="<?php echo $this->lang->line('player_player'); ?>"><?php echo anchor('/player/view/id/' . $player->id, Player_helper::fullNameReverse($player)); ?></td>
+            <td data-title="<?php echo $this->lang->line('player_d_o_b'); ?>"><?php echo Utility_helper::formattedDate($player->dob, "jS M Y"); ?></td>
+            <td data-title="<?php echo $this->lang->line('player_apps'); ?>"><?php echo $player->appearances; ?> (<?php echo $player->substitute_appearances; ?>)</td>
+            <td data-title="<?php echo $this->lang->line('player_goals'); ?>"><?php echo $player->goals; ?></td>
+            <td data-title="<?php echo $this->lang->line('player_assists'); ?>"><?php echo $player->assists; ?></td>
+            <td data-title="<?php echo $this->lang->line('player_motms'); ?>"><?php echo $player->motms; ?></td>
+            <td data-title="<?php echo $this->lang->line('player_yellows'); ?>"><?php echo $player->yellows; ?></td>
+            <td data-title="<?php echo $this->lang->line('player_reds'); ?>"><?php echo $player->reds; ?></td>
             <?php
             if (Configuration::get('include_appearance_ratings') === true) { ?>
-            <td><?php echo Player_helper::rating($player->average_rating); ?></td>
+            <td data-title="<?php echo $this->lang->line('player_rating'); ?>"><?php echo Player_helper::rating($player->average_rating); ?></td>
             <?php
             } ?>
         </tr>
