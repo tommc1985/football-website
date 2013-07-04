495306d60c5a3986f4742591645bab57a3357fe7
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-07-04 11:22:17 +0100

    Renamed responsive-tables.css to tables.css

--- application/views/themes/default/header.php
+++ application/views/themes/default/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/bootstrap/docs/assets/css/bootstrap.css" rel="stylesheet">
     <link href="/bootstrap/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
-    <link href="/assets/css/responsive-tables.css" rel="stylesheet">
+    <link href="/assets/css/tables.css" rel="stylesheet">
     <link href="/assets/modules/fantasy-football/css/fantasy-football.css" rel="stylesheet">
 
 
