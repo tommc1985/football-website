66fbbbeec7261de4b586a4ed55d17fb1ef10acb0
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-07-25 22:21:26 +0100

    Changes to header, remove CSS file

--- application/views/themes/default/header.php
+++ application/views/themes/default/header.php
@@ -11,100 +11,100 @@
     <link href="<?php echo site_url("assets/modules/fantasy-football/css/fantasy-football.css"); ?>" rel="stylesheet">
 
 
-    <link href="<?php echo site_url("bootstrap/docs/assets/css/docs.css"); ?>" rel="stylesheet">
+    <?php /*<link href="<?php echo site_url("bootstrap/docs/assets/css/docs.css"); ?>" rel="stylesheet">*/ ?>
     <link href="<?php echo site_url("assets/themes/default/css/style.css"); ?>" rel="stylesheet">
 </head>
 <body>
 
 <div class="navbar navbar-fixed-top">
-      <div class="navbar-inner">
+    <div class="navbar-inner">
         <div class="container">
-          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <div class="nav-collapse collapse">
-            <ul class="nav">
-                <li>
-                    <a href="<?php echo site_url(); ?>">Home</a>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Club Info<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php echo site_url("club-history"); ?>">History</a></li>
-                        <li><a href="<?php echo site_url("club-officials"); ?>">Officials</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">News &amp; Articles<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php echo site_url("news"); ?>">News</a></li>
-                        <li><a href="<?php echo site_url("article"); ?>">Articles</a></li>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Matchday<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php echo site_url("player"); ?>">Squad List</a></li>
-                        <li><a href="<?php echo site_url("match"); ?>">Fixtures &amp; Results</a></li>
-                        <?php
-                        $menuLeagues = $this->League_model->fetchAll(array(
-                            'season' => Season_model::fetchCurrentSeason()));
-                        foreach ($menuLeagues as $menuLeague) { ?>
-                        <li><a href="<?php echo site_url("league/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> League Table</a></li>
-                        <?php
-                        } ?>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Statistics<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="<?php echo site_url("club-statistics/view"); ?>">Club Statistics</a></li>
-                        <li><a href="<?php echo site_url("player-statistics/view"); ?>">Player Statistics</a></li>
-                        <li><a href="<?php echo site_url("fantasy-football/view"); ?>">Fantasy Football</a></li>
-                        <li><a href="<?php echo site_url("head-to-head"); ?>">Head to Head</a></li>
-                        <?php
-                        foreach ($menuLeagues as $menuLeague) { ?>
-                        <li><a href="<?php echo site_url("league-statistics/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> Statistics</a></li>
-                        <?php
-                        } ?>
-                    </ul>
-                </li>
-                <li class="dropdown">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Archive<b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                    <?php
-                    foreach ((array('all-time' => 'All Time') + $this->Season_model->fetchForDropdown())  as $menuSeason => $menuSeasonFriendly) {
-                        if ($menuSeason != Season_model::fetchCurrentSeason()) { ?>
-                        <li class="dropdown-submenu">
-                            <a tabindex="-1" href="#"><?php echo $menuSeasonFriendly; ?></a>
+            <a class="brand" href="<?php echo site_url(); ?>"><?php echo Configuration::get('team_name'); ?></a>
+            <div class="container">
+                <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <div class="nav-collapse collapse">
+                    <ul class="nav">
+                        <li>
+                            <a href="<?php echo site_url(); ?>">Home</a>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Club Info<b class="caret"></b></a>
                             <ul class="dropdown-menu">
-                                <li><a href="<?php echo site_url("match/index/season/{$menuSeason}"); ?>">Fixtures &amp; Results</a></li>
-                                <li><a href="<?php echo site_url("club-statistics/view/season/{$menuSeason}"); ?>">Club Statistics</a></li>
-                                <li><a href="<?php echo site_url("player-statistics/view/season/{$menuSeason}"); ?>">Player Statistics</a></li>
-                                <li><a href="<?php echo site_url("fantasy-football/view/season/{$menuSeason}"); ?>">Fantasy Football</a></li>
+                                <li><a href="<?php echo site_url("club-history"); ?>">History</a></li>
+                                <li><a href="<?php echo site_url("club-officials"); ?>">Officials</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">News &amp; Articles<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo site_url("news"); ?>">News</a></li>
+                                <li><a href="<?php echo site_url("article"); ?>">Articles</a></li>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Matchday<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo site_url("player"); ?>">Squad List</a></li>
+                                <li><a href="<?php echo site_url("match"); ?>">Fixtures &amp; Results</a></li>
                                 <?php
                                 $menuLeagues = $this->League_model->fetchAll(array(
-                                    'season' => $menuSeason));
+                                    'season' => Season_model::fetchCurrentSeason()));
                                 foreach ($menuLeagues as $menuLeague) { ?>
-                                <li><a href="<?php echo site_url("league-statistics/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> Statistics</a></li>
                                 <li><a href="<?php echo site_url("league/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> League Table</a></li>
                                 <?php
                                 } ?>
                             </ul>
                         </li>
-                    <?php
-                        }
-                    } ?>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Statistics<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?php echo site_url("club-statistics/view"); ?>">Club Statistics</a></li>
+                                <li><a href="<?php echo site_url("player-statistics/view"); ?>">Player Statistics</a></li>
+                                <li><a href="<?php echo site_url("fantasy-football/view"); ?>">Fantasy Football</a></li>
+                                <li><a href="<?php echo site_url("head-to-head"); ?>">Head to Head</a></li>
+                                <?php
+                                foreach ($menuLeagues as $menuLeague) { ?>
+                                <li><a href="<?php echo site_url("league-statistics/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> Statistics</a></li>
+                                <?php
+                                } ?>
+                            </ul>
+                        </li>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" data-target="#" href="#">Archive<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                            <?php
+                            foreach ((array('all-time' => 'All Time') + $this->Season_model->fetchForDropdown())  as $menuSeason => $menuSeasonFriendly) {
+                                if ($menuSeason != Season_model::fetchCurrentSeason()) { ?>
+                                <li class="dropdown-submenu">
+                                    <a tabindex="-1" href="#"><?php echo $menuSeasonFriendly; ?></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="<?php echo site_url("match/index/season/{$menuSeason}"); ?>">Fixtures &amp; Results</a></li>
+                                        <li><a href="<?php echo site_url("club-statistics/view/season/{$menuSeason}"); ?>">Club Statistics</a></li>
+                                        <li><a href="<?php echo site_url("player-statistics/view/season/{$menuSeason}"); ?>">Player Statistics</a></li>
+                                        <li><a href="<?php echo site_url("fantasy-football/view/season/{$menuSeason}"); ?>">Fantasy Football</a></li>
+                                        <?php
+                                        $menuLeagues = $this->League_model->fetchAll(array(
+                                            'season' => $menuSeason));
+                                        foreach ($menuLeagues as $menuLeague) { ?>
+                                        <li><a href="<?php echo site_url("league-statistics/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> Statistics</a></li>
+                                        <li><a href="<?php echo site_url("league/view/id/{$menuLeague->id}"); ?>"><?php echo $menuLeague->short_name; ?> League Table</a></li>
+                                        <?php
+                                        } ?>
+                                    </ul>
+                                </li>
+                            <?php
+                                }
+                            } ?>
+                            </ul>
+                        </li>
                     </ul>
-                </li>
-            </ul>
-          </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
+</div>
 <div class="container">
-    <div class="page-header">
-        <h1><?php echo Configuration::get('team_name'); ?> <small>...</small></h1>
-    </div>
