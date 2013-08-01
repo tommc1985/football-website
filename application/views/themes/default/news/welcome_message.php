bda19b71ba23e85f2e41492adc739dd5d28e62be
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-01 22:40:38 +0100

    Changed layout of Article and News frontend pages

--- application/views/themes/default/news/welcome_message.php
+++ application/views/themes/default/news/welcome_message.php
@@ -8,15 +8,30 @@
         </div>
 
         <?php
-        if ($articles) {
-            foreach ($articles as $article) { ?>
-            <div itemscope itemtype="http://schema.org/NewsArticle">
-                <h3 itemprop="headline"><?php echo $article->title; ?></h3>
-                <p><time itemprop="datePublished" datetime="<?php echo Utility_helper::formattedDate($article->publish_date, "c"); ?>"><?php echo Utility_helper::shortDate($article->publish_date); ?></time></p>
-                <p itemprop="articleSection"><?php echo Utility_helper::partialContent($article->content, 250, " "); ?> (<a href="<?php echo site_url("/news/view/id/{$article->id}"); ?>">read more</a>)</p>
+        if ($articles) { ?>
+                <div class="row-fluid">
+                <?php
+                $i = 0;
+                foreach ($articles as $article) { ?>
+                <div class="span6" itemscope itemtype="http://schema.org/NewsArticle">
+                    <h3 itemprop="headline"><?php echo $article->title; ?></h3>
+                    <p><time itemprop="datePublished" datetime="<?php echo Utility_helper::formattedDate($article->publish_date, "c"); ?>"><?php echo Utility_helper::shortDate($article->publish_date); ?></time></p>
+                    <p itemprop="articleSection"><?php echo Utility_helper::partialContent($article->content, 250, " "); ?> (<a href="<?php echo site_url("/news/view/id/{$article->id}"); ?>">read more</a>)</p>
+                </div>
+            <?php
+                    if (($i % 2) == 1) { ?>
             </div>
-        <?php
-            }
+            <div class="row-fluid">
+                    <?php
+                    }
+
+                    $i++;
+                }
+
+                if (($i % 2) == 0) { ?>
+            </div>
+                    <?php
+                }
         } else { ?>
             <?php echo $this->lang->line('news_no_news_found'); ?></td>
         <?php
