8804425dfa9402f61f14d8ab2eb4e3f9dd154d9b
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-01 22:41:14 +0100

    Change layout of Player listing page

--- application/views/themes/default/player/welcome_message.php
+++ application/views/themes/default/player/welcome_message.php
@@ -2,20 +2,20 @@
     <div class="span12">
         <h2><?php echo $this->lang->line('player_squad_list'); ?></h2>
 
-        <table class="no-more-tables">
+        <table class="no-more-tables width-100-percent">
             <thead>
                 <tr>
                     <td><?php echo $this->lang->line('player_player'); ?></td>
-                    <td><?php echo $this->lang->line('player_d_o_b'); ?></td>
-                    <td><?php echo $this->lang->line('player_apps'); ?></td>
-                    <td><?php echo $this->lang->line('player_goals'); ?></td>
-                    <td><?php echo $this->lang->line('player_assists'); ?></td>
-                    <td><?php echo $this->lang->line('player_motms'); ?></td>
-                    <td><?php echo $this->lang->line('player_yellows'); ?></td>
-                    <td><?php echo $this->lang->line('player_reds'); ?></td>
+                    <td class="width-15-percent"><?php echo $this->lang->line('player_d_o_b'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_apps'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_goals'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_assists'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_motms'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_yellows'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_reds'); ?></td>
                     <?php
                     if (Configuration::get('include_appearance_ratings') === true) { ?>
-                    <td><?php echo $this->lang->line('player_rating'); ?></td>
+                    <td class="width-10-percent"><?php echo $this->lang->line('player_rating'); ?></td>
                     <?php
                     } ?>
                 </tr>
@@ -27,13 +27,13 @@
             foreach ($players as $player) { ?>
                 <tr itemscope itemtype="http://schema.org/Person">
                     <td itemprop="name" data-title="<?php echo $this->lang->line('player_player'); ?>"><?php echo Player_helper::fullNameReverse($player); ?></td>
-                    <td data-title="<?php echo $this->lang->line('player_d_o_b'); ?>"><time itemprop="birthDate" datetime="<?php echo Utility_helper::formattedDate($player->dob, "c"); ?>"><?php echo Utility_helper::formattedDate($player->dob, "jS M Y"); ?></time></td>
-                    <td data-title="<?php echo $this->lang->line('player_apps'); ?>"><?php echo $player->appearances; ?> (<?php echo $player->substitute_appearances; ?>)</td>
-                    <td data-title="<?php echo $this->lang->line('player_goals'); ?>"><?php echo $player->goals; ?></td>
-                    <td data-title="<?php echo $this->lang->line('player_assists'); ?>"><?php echo $player->assists; ?></td>
-                    <td data-title="<?php echo $this->lang->line('player_motms'); ?>"><?php echo $player->motms; ?></td>
-                    <td data-title="<?php echo $this->lang->line('player_yellows'); ?>"><?php echo $player->yellows; ?></td>
-                    <td data-title="<?php echo $this->lang->line('player_reds'); ?>"><?php echo $player->reds; ?></td>
+                    <td data-title="<?php echo $this->lang->line('player_d_o_b'); ?>" class="text-align-center"><time itemprop="birthDate" datetime="<?php echo Utility_helper::formattedDate($player->dob, "c"); ?>"><?php echo Utility_helper::formattedDate($player->dob, "jS M Y"); ?></time></td>
+                    <td data-title="<?php echo $this->lang->line('player_apps'); ?>" class="text-align-center"><?php echo $player->appearances; ?> (<?php echo $player->substitute_appearances; ?>)</td>
+                    <td data-title="<?php echo $this->lang->line('player_goals'); ?>" class="text-align-center"><?php echo $player->goals; ?></td>
+                    <td data-title="<?php echo $this->lang->line('player_assists'); ?>" class="text-align-center"><?php echo $player->assists; ?></td>
+                    <td data-title="<?php echo $this->lang->line('player_motms'); ?>" class="text-align-center"><?php echo $player->motms; ?></td>
+                    <td data-title="<?php echo $this->lang->line('player_yellows'); ?>" class="text-align-center"><?php echo $player->yellows; ?></td>
+                    <td data-title="<?php echo $this->lang->line('player_reds'); ?>" class="text-align-center"><?php echo $player->reds; ?></td>
                     <?php
                     if (Configuration::get('include_appearance_ratings') === true) { ?>
                     <td data-title="<?php echo $this->lang->line('player_rating'); ?>"><?php echo Player_helper::rating($player->average_rating); ?></td>
