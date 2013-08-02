3fdfdd4b58995a5347719776c10376b8675770e4
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-02 15:31:04 +0100

    Changed Competition form to use bootstrap

--- application/language/english/competition_lang.php
+++ application/language/english/competition_lang.php
@@ -1,28 +1,30 @@
 <?php
 
 // Errors
-$lang['competition_not_found']     = 'The specified Competition cannot be found.';
-$lang['competition_cannot_delete'] = "Cannot delete the '%s' Competition, there are Leagues and/or Matches still linked to it.";
+$lang['competition_not_found']               = 'The specified Competition cannot be found.';
+$lang['competition_cannot_delete']           = "Cannot delete the '%s' Competition, there are Leagues and/or Matches still linked to it.";
 
 // Form Labels
-$lang['competition_add']  = 'Add Competition';
-$lang['competition_save'] = 'Save Competition Info';
+$lang['competition_add']                     = 'Add Competition';
+$lang['competition_save']                    = 'Save Competition Info';
 
-$lang['competition_name']         = 'Name';
-$lang['competition_short_name']   = 'Short Name';
-$lang['competition_abbreviation'] = 'Abbreviation';
-$lang['competition_type']         = 'Type';
-$lang['competition_starts']       = 'Starts';
-$lang['competition_subs']         = 'Subs';
-$lang['competition_competitive']  = 'Competitive';
+$lang['competition_competition_details']      = 'Competition Details';
+
+$lang['competition_name']                    = 'Name';
+$lang['competition_short_name']              = 'Short Name';
+$lang['competition_abbreviation']            = 'Abbreviation';
+$lang['competition_type']                    = 'Type';
+$lang['competition_starts']                  = 'Starts';
+$lang['competition_subs']                    = 'Subs';
+$lang['competition_competitive']             = 'Competitive';
 
 // Validation Errors
 
 
 // Messages
-$lang['competition_added']   = "Competition '%s' has been added";
-$lang['competition_updated'] = "Competition '%s' has been updated";
-$lang['competition_deleted'] = "Competition '%s' has been deleted";
+$lang['competition_added']                   = "Competition '%s' has been added";
+$lang['competition_updated']                 = "Competition '%s' has been updated";
+$lang['competition_deleted']                 = "Competition '%s' has been deleted";
 
 // Text
 $lang['competition_confirm_delete_question'] = "Are you sure you want to delete the '%s' Competition?";
--- application/views/admin/competition/form.php
+++ application/views/admin/competition/form.php
@@ -4,91 +4,153 @@ $this->load->model('Competition_model');
 $id = array(
     'name'  => 'id',
     'id'    => 'id',
-    'value' => set_value('id'),
+    'value' => set_value('id', isset($competition->id) ? $competition->id : ''),
 );
 
 $name = array(
-    'name'    => 'name',
-    'id'      => 'name',
-    'value'   => set_value('name'),
+    'name'        => 'name',
+    'id'          => 'name',
+    'value'       => set_value('name', isset($competition->name) ? $competition->name : ''),
+    'placeholder' => $this->lang->line('competition_name'),
+    'class'       => 'input-xlarge',
 );
 
-$short_name = array(
-    'name'    => 'short_name',
-    'id'      => 'short_name',
-    'value'   => set_value('short_name'),
+$shortName = array(
+    'name'        => 'short_name',
+    'id'          => 'short-name',
+    'value'       => set_value('short_name', isset($competition->short_name) ? $competition->short_name : ''),
+    'placeholder' => $this->lang->line('competition_short_name'),
+    'class'       => 'input-large',
 );
 
 $abbreviation = array(
-    'name'    => 'abbreviation',
-    'id'      => 'abbreviation',
-    'value'   => set_value('abbreviation'),
+    'name'        => 'abbreviation',
+    'id'          => 'abbreviation',
+    'value'       => set_value('abbreviation', isset($competition->abbreviation) ? $competition->abbreviation : ''),
+    'placeholder' => $this->lang->line('competition_abbreviation'),
+    'class'       => 'input-small',
 );
 
 $type = array(
-    'name'    => 'type',
-    'id'      => 'type',
-    'options' => array('' => '--- Select ---') + Competition_model::fetchTypes(),
-    'value'   => set_value('type'),
+    'name'       => 'type',
+    'id'         => 'type',
+    'options'    => array('' => '--- Select ---') + Competition_model::fetchTypes(),
+    'value'      => set_value('type', isset($competition->type) ? $competition->type : ''),
+    'attributes' => 'class="input-medium"',
 );
 
 $starts = array(
-    'name'  => 'starts',
-    'id'    => 'starts',
-    'value' => set_value('starts'),
+    'name'        => 'starts',
+    'id'          => 'starts',
+    'value'       => set_value('starts', isset($competition->starts) ? $competition->starts : ''),
+    'placeholder' => $this->lang->line('competition_starts'),
+    'class'       => 'input-mini',
 );
 
 $subs = array(
-    'name'  => 'subs',
-    'id'    => 'subs',
-    'value' => set_value('subs'),
+    'name'        => 'subs',
+    'id'          => 'subs',
+    'value'       => set_value('subs', isset($competition->subs) ? $competition->subs : ''),
+    'placeholder' => $this->lang->line('competition_subs'),
+    'class'       => 'input-mini',
 );
 
 $competitive = array(
-    'name'  => 'competitive',
-    'id'    => 'competitive',
-    'options' => array('' => '--- Select ---') + Competition_model::fetchCompetitive(),
-    'value' => set_value('competitive'),
+    'name'       => 'competitive',
+    'id'         => 'competitive',
+    'options'    => array('' => '--- Select ---') + Competition_model::fetchCompetitive(),
+    'value'      => set_value('type', isset($competition->competitive) ? $competition->competitive : ''),
+    'attributes' => 'class="input-medium"',
 );
 
+$submit = array(
+    'name'  => 'submit',
+    'class' => 'btn',
+    'value' => $submitButtonText,
+); ?>
+
+<?php
 echo form_open($this->uri->uri_string()); ?>
-<table>
-    <?php echo form_hidden('id', set_value('id', isset($competition->id) ? $competition->id : '')); ?>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_name'), $name['name']); ?></td>
-        <td><?php echo form_input($name['name'], set_value($name['name'], isset($competition->name) ? $competition->name : '')); ?></td>
-        <td class="error"><?php echo form_error($name['name']); ?><?php echo isset($errors[$name['name']]) ? $errors[$name['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_short_name'), $short_name['name']); ?></td>
-        <td><?php echo form_input($short_name['name'], set_value($short_name['name'], isset($competition->short_name) ? $competition->short_name : '')); ?></td>
-        <td class="error"><?php echo form_error($short_name['name']); ?><?php echo isset($errors[$short_name['name']]) ? $errors[$short_name['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_abbreviation'), $abbreviation['name']); ?></td>
-        <td><?php echo form_input($abbreviation['name'], set_value($abbreviation['name'], isset($competition->abbreviation) ? $competition->abbreviation : '')); ?></td>
-        <td class="error"><?php echo form_error($abbreviation['name']); ?><?php echo isset($errors[$abbreviation['name']]) ? $errors[$abbreviation['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_type'), $type['name']); ?></td>
-        <td><?php echo form_dropdown($type['name'], $type['options'], set_value($type['name'], isset($competition->type) ? $competition->type : '')); ?></td>
-        <td class="error"><?php echo form_error($type['name']); ?><?php echo isset($errors[$type['name']]) ? $errors[$type['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_starts'), $starts['name']); ?></td>
-        <td><?php echo form_input($starts['name'], set_value($starts['name'], isset($competition->starts) ? $competition->starts : '')); ?></td>
-        <td class="error"><?php echo form_error($starts['name']); ?><?php echo isset($errors[$starts['name']]) ? $errors[$starts['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_subs'), $subs['name']); ?></td>
-        <td><?php echo form_input($subs['name'], set_value($subs['name'], isset($competition->subs) ? $competition->subs : '')); ?></td>
-        <td class="error"><?php echo form_error($subs['name']); ?><?php echo isset($errors[$subs['name']]) ? $errors[$subs['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('competition_competitive'), $competitive['name']); ?></td>
-        <td><?php echo form_dropdown($competitive['name'], $competitive['options'], set_value($competitive['name'], isset($competition->competitive) ? $competition->competitive : '')); ?></td>
-        <td class="error"><?php echo form_error($competitive['name']); ?><?php echo isset($errors[$competitive['name']]) ? $errors[$competitive['name']] : ''; ?></td>
-    </tr>
-</table>
-<?php echo form_submit('submit', $submitButtonText); ?>
+    <?php echo form_hidden($id['name'], $id['value']); ?>
+    <fieldset>
+        <legend><?php echo $this->lang->line('competition_competition_details');?></legend>
+        <div class="control-group<?php echo form_error($name['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_name'), $name['id']); ?>
+            <div class="controls">
+                <?php echo form_input($name); ?>
+                <?php
+                if (form_error($name['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($name['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($shortName['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_short_name'), $shortName['id']); ?>
+            <div class="controls">
+                <?php echo form_input($shortName); ?>
+                <?php
+                if (form_error($shortName['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($shortName['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($abbreviation['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_abbreviation'), $abbreviation['id']); ?>
+            <div class="controls">
+                <?php echo form_input($abbreviation); ?>
+                <?php
+                if (form_error($abbreviation['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($abbreviation['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($type['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_type'), $type['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($type['name'], $type['options'], $type['value'], $type['attributes']); ?>
+                <?php
+                if (form_error($type['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($type['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($starts['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_starts'), $starts['id']); ?>
+            <div class="controls">
+                <?php echo form_input($starts); ?>
+                <?php
+                if (form_error($starts['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($starts['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($subs['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_subs'), $subs['id']); ?>
+            <div class="controls">
+                <?php echo form_input($subs); ?>
+                <?php
+                if (form_error($subs['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($subs['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($competitive['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('competition_competitive'), $competitive['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($competitive['name'], $competitive['options'], $competitive['value'], $competitive['attributes']); ?>
+                <?php
+                if (form_error($competitive['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($competitive['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <?php echo form_submit($submit); ?>
+    </fieldset>
 <?php echo form_close(); ?>
