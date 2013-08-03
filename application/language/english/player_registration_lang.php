05d8b18dff5ce5200021e038ba298be314bd09d2
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-03 16:05:03 +0100

    Applied Bootstrap to Player Registration form

--- application/language/english/player_registration_lang.php
+++ application/language/english/player_registration_lang.php
@@ -1,32 +1,34 @@
 <?php
 
 // Errors
-$lang['player_registration_not_found']     = 'The specified Player Registration cannot be found.';
-$lang['player_registration_cannot_delete'] = 'Cannot delete this Player Registration, there are Player Matches still linked to it.';
+$lang['player_registration_not_found']                   = 'The specified Player Registration cannot be found.';
+$lang['player_registration_cannot_delete']               = 'Cannot delete this Player Registration, there are Player Matches still linked to it.';
 
 // Form Labels
-$lang['player_registration_add']  = 'Add Player Registration';
-$lang['player_registration_save'] = 'Save Player Registration Details';
+$lang['player_registration_add']                         = 'Add Player Registration';
+$lang['player_registration_save']                        = 'Save Player Registration Details';
 
-$lang['player_registration_player'] = 'Player';
-$lang['player_registration_season'] = 'Season';
+$lang['player_registration_player_registration_details'] = 'Player Registration Details';
+
+$lang['player_registration_player']                      = 'Player';
+$lang['player_registration_season']                      = 'Season';
 
 // Validation Errors
 
 
 // Messages
-$lang['player_registration_added']   = "Player Registration '%s' has been added";
-$lang['player_registration_updated'] = "Player Registration '%s' has been updated";
-$lang['player_registration_deleted'] = "Player Registration '%s' has been deleted";
+$lang['player_registration_added']                       = "Player Registration '%s' has been added";
+$lang['player_registration_updated']                     = "Player Registration '%s' has been updated";
+$lang['player_registration_deleted']                     = "Player Registration '%s' has been deleted";
 
 // Text
-$lang['player_registration_confirm_delete_question'] = "Are you sure you want to delete the Player Registration '%s'?";
-$lang['player_registration_confirm_delete_yes']      = "Yes please";
-$lang['player_registration_confirm_delete_no']       = "No thanks";
-$lang['player_registration_edit']                    = 'Edit';
-$lang['player_registration_delete']                  = 'Delete';
+$lang['player_registration_confirm_delete_question']     = "Are you sure you want to delete the Player Registration '%s'?";
+$lang['player_registration_confirm_delete_yes']          = "Yes please";
+$lang['player_registration_confirm_delete_no']           = "No thanks";
+$lang['player_registration_edit']                        = 'Edit';
+$lang['player_registration_delete']                      = 'Delete';
 
 
-$lang['player_registration_no_player_registrations'] = 'No Player Registrations exist';
+$lang['player_registration_no_player_registrations']     = 'No Player Registrations exist';
 /* End of file player_registration.php */
 /* Location: ./application/language/english/player_registration.php */
--- application/views/admin/player-registration/form.php
+++ application/views/admin/player-registration/form.php
@@ -2,36 +2,58 @@
 $id = array(
     'name'  => 'id',
     'id'    => 'id',
-    'value' => set_value('id'),
+    'value' => set_value('id', isset($playerRegistration->id) ? $playerRegistration->id : ''),
 );
 
-$player_id = array(
+$playerId = array(
     'name'    => 'player_id',
     'id'      => 'player_id',
     'options' => array('' => '--- Select ---') + $this->Player_model->fetchForDropdown(),
-    'value'   => set_value('player_id'),
+    'value'   => set_value('player_id', isset($playerRegistration->player_id) ? $playerRegistration->player_id : ''),
+    'attributes' => 'class="input-xlarge"',
 );
 
 $season = array(
     'name'    => 'season',
     'id'      => 'season',
     'options' => array('' => '--- Select ---') + $this->Season_model->fetchForDropdown(),
-    'value'   => set_value('season'),
+    'value'   => set_value('season', isset($playerRegistration->season) ? $playerRegistration->season : ''),
+    'attributes' => 'class="input-medium"',
 );
 
+$submit = array(
+    'name'  => 'submit',
+    'class' => 'btn',
+    'value' => $submitButtonText,
+); ?>
+
+<?php
 echo form_open($this->uri->uri_string()); ?>
-<table>
-    <?php echo form_hidden('id', set_value('id', isset($playerRegistration->id) ? $playerRegistration->id : '')); ?>
-    <tr>
-        <td><?php echo form_label($this->lang->line('player_registration_player'), $player_id['name']); ?></td>
-        <td><?php echo form_dropdown($player_id['name'], $player_id['options'], set_value($player_id['name'], isset($playerRegistration->player_id) ? $playerRegistration->player_id : '')); ?></td>
-        <td class="error"><?php echo form_error($player_id['name']); ?><?php echo isset($errors[$player_id['name']]) ? $errors[$player_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('player_registration_season'), $season['name']); ?></td>
-        <td><?php echo form_dropdown($season['name'], $season['options'], set_value($season['name'], isset($playerRegistration->season) ? $playerRegistration->season : '')); ?></td>
-        <td class="error"><?php echo form_error($season['name']); ?><?php echo isset($errors[$season['name']]) ? $errors[$season['name']] : ''; ?></td>
-    </tr>
-</table>
-<?php echo form_submit('submit', $submitButtonText); ?>
+    <?php echo form_hidden($id['name'], $id['value']); ?>
+    <fieldset>
+        <legend><?php echo $this->lang->line('player_registration_player_registration_details');?></legend>
+        <div class="control-group<?php echo form_error($playerId['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('player_registration_player'), $playerId['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($playerId['name'], $playerId['options'], $playerId['value'], $playerId['attributes']); ?>
+                <?php
+                if (form_error($playerId['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($playerId['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($season['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('player_registration_season'), $season['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($season['name'], $season['options'], $season['value'], $season['attributes']); ?>
+                <?php
+                if (form_error($season['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($season['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <?php echo form_submit($submit); ?>
+    </fieldset>
 <?php echo form_close(); ?>
