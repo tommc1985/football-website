c1b3a8c3c88e2ecfc209b7c07cb541e356bacd62
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-03 14:52:53 +0100

    Applied Bootstrap to Official form

--- application/language/english/official_lang.php
+++ application/language/english/official_lang.php
@@ -1,24 +1,26 @@
 <?php
 
 // Errors
-$lang['official_not_found']     = 'The specified Official cannot be found.';
-$lang['official_cannot_delete'] = "Cannot delete Official '%s', there are Matches still linked to them.";
+$lang['official_not_found']               = 'The specified Official cannot be found.';
+$lang['official_cannot_delete']           = "Cannot delete Official '%s', there are Matches still linked to them.";
 
 // Form Labels
-$lang['official_add']  = 'Add Official';
-$lang['official_save'] = 'Save Official Info';
+$lang['official_add']                     = 'Add Official';
+$lang['official_save']                    = 'Save Official Info';
 
-$lang['official_first_name'] = 'First Name';
-$lang['official_surname']    = 'Surname';
-$lang['official_name']       = 'Name';
+$lang['official_official_details']        = 'Official Details';
+
+$lang['official_first_name']              = 'First Name';
+$lang['official_surname']                 = 'Surname';
+$lang['official_name']                    = 'Name';
 
 // Validation Errors
 
 
 // Messages
-$lang['official_added']   = "Official '%s' has been added";
-$lang['official_updated'] = "Official '%s' has been updated";
-$lang['official_deleted'] = "Official '%s' has been deleted";
+$lang['official_added']                   = "Official '%s' has been added";
+$lang['official_updated']                 = "Official '%s' has been updated";
+$lang['official_deleted']                 = "Official '%s' has been deleted";
 
 // Text
 $lang['official_confirm_delete_question'] = "Are you sure you want to delete the Official '%s'?";
--- application/views/admin/official/form.php
+++ application/views/admin/official/form.php
@@ -2,36 +2,60 @@
 $id = array(
     'name'  => 'id',
     'id'    => 'id',
-    'value' => set_value('id'),
+    'value' => set_value('id', isset($official->id) ? $official->id : ''),
 );
 
-$first_name = array(
-    'name'  => 'first_name',
-    'id'    => 'first-name',
-    'value' => set_value('first_name'),
-    'maxlength' => $this->config->item('first_name_max_length', 'official'),
+$firstName = array(
+    'name'        => 'first_name',
+    'id'          => 'first-name',
+    'value'       => set_value('first_name', isset($official->first_name) ? $official->first_name : ''),
+    'maxlength'   => $this->config->item('first_name_max_length', 'official'),
+    'placeholder' => $this->lang->line('official_first_name'),
+    'class'       => 'input-xlarge',
 );
 
 $surname = array(
-    'name'  => 'surname',
-    'id'    => 'surname',
-    'value' => set_value('surname'),
-    'maxlength' => $this->config->item('surname_max_length', 'official'),
+    'name'        => 'surname',
+    'id'          => 'surname',
+    'value'       => set_value('surname', isset($official->surname) ? $official->surname : ''),
+    'maxlength'   => $this->config->item('surname_max_length', 'official'),
+    'placeholder' => $this->lang->line('official_surname'),
+    'class'       => 'input-xlarge',
 );
 
+$submit = array(
+    'name'  => 'submit',
+    'class' => 'btn',
+    'value' => $submitButtonText,
+); ?>
+
+<?php
 echo form_open($this->uri->uri_string()); ?>
-<table>
-    <?php echo form_hidden('id', set_value('id', isset($official->id) ? $official->id : '')); ?>
-    <tr>
-        <td><?php echo form_label($this->lang->line('official_first_name'), 'first_name'); ?></td>
-        <td><?php echo form_input('first_name', set_value('first_name', isset($official->first_name) ? $official->first_name : '')); ?></td>
-        <td class="error"><?php echo form_error($first_name['name']); ?><?php echo isset($errors[$first_name['name']]) ? $errors[$first_name['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('official_surname'), 'surname'); ?></td>
-        <td><?php echo form_input('surname', set_value('surname', isset($official->surname) ? $official->surname : '')); ?></td>
-        <td class="error"><?php echo form_error($surname['name']); ?><?php echo isset($errors[$surname['name']]) ? $errors[$surname['name']] : ''; ?></td>
-    </tr>
-</table>
-<?php echo form_submit('submit', $submitButtonText); ?>
+    <?php echo form_hidden($id['name'], $id['value']); ?>
+    <fieldset>
+        <legend><?php echo $this->lang->line('official_official_details');?></legend>
+        <div class="control-group<?php echo form_error($firstName['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('official_first_name'), $firstName['id']); ?>
+            <div class="controls">
+                <?php echo form_input($firstName); ?>
+                <?php
+                if (form_error($firstName['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($firstName['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($surname['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('official_surname'), $surname['id']); ?>
+            <div class="controls">
+                <?php echo form_input($surname); ?>
+                <?php
+                if (form_error($surname['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($surname['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <?php echo form_submit($submit); ?>
+    </fieldset>
 <?php echo form_close(); ?>
