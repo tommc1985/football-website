820131c87516418dcb5db76107659fe5928ac5d4
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-03 13:52:56 +0100

    Applied Bootstrap to League Registration form

--- application/language/english/league_registration_lang.php
+++ application/language/english/league_registration_lang.php
@@ -1,31 +1,33 @@
 <?php
 
 // Errors
-$lang['league_registration_not_found']     = 'The specified League Registration cannot be found.';
-$lang['league_registration_cannot_delete'] = 'Cannot delete this League Registration, there are League Matches still linked to it.';
+$lang['league_registration_not_found']                   = 'The specified League Registration cannot be found.';
+$lang['league_registration_cannot_delete']               = 'Cannot delete this League Registration, there are League Matches still linked to it.';
 
 // Form Labels
-$lang['league_registration_add']  = 'Add League Registration';
-$lang['league_registration_save'] = 'Save League Registration Details';
+$lang['league_registration_add']                         = 'Add League Registration';
+$lang['league_registration_save']                        = 'Save League Registration Details';
 
-$lang['league_registration_league']     = 'League';
-$lang['league_registration_team']       = 'Team';
+$lang['league_registration_league_registration_details'] = 'League Registration Details';
+
+$lang['league_registration_league']                      = 'League';
+$lang['league_registration_team']                        = 'Team';
 
 // Validation Errors
 
 
 // Messages
-$lang['league_registration_added']   = "League Registration '%s' has been added";
-$lang['league_registration_updated'] = "League Registration '%s' has been updated";
-$lang['league_registration_deleted'] = "League Registration '%s' has been deleted";
+$lang['league_registration_added']                       = "League Registration '%s' has been added";
+$lang['league_registration_updated']                     = "League Registration '%s' has been updated";
+$lang['league_registration_deleted']                     = "League Registration '%s' has been deleted";
 
 // Text
-$lang['league_registration_confirm_delete_question'] = "Are you sure you want to delete the League Registration '%s'?";
-$lang['league_registration_confirm_delete_yes']      = "Yes please";
-$lang['league_registration_confirm_delete_no']       = "No thanks";
-$lang['league_registration_edit']                    = 'Edit';
-$lang['league_registration_delete']                  = 'Delete';
+$lang['league_registration_confirm_delete_question']     = "Are you sure you want to delete the League Registration '%s'?";
+$lang['league_registration_confirm_delete_yes']          = "Yes please";
+$lang['league_registration_confirm_delete_no']           = "No thanks";
+$lang['league_registration_edit']                        = 'Edit';
+$lang['league_registration_delete']                      = 'Delete';
 
-$lang['league_registration_no_league_registrations'] = 'No League Registrations exist';
+$lang['league_registration_no_league_registrations']     = 'No League Registrations exist';
 /* End of file league_registration.php */
 /* Location: ./application/language/english/league_registration.php */
--- application/views/admin/league-registration/form.php
+++ application/views/admin/league-registration/form.php
@@ -2,36 +2,58 @@
 $id = array(
     'name'  => 'id',
     'id'    => 'id',
-    'value' => set_value('id'),
+    'value' => set_value('id', isset($leagueRegistration->id) ? $leagueRegistration->id : ''),
 );
 
-$league_id = array(
-    'name'    => 'league_id',
-    'id'      => 'league_id',
-    'options' => array('' => '--- Select ---') + $this->League_model->fetchForDropdown(),
-    'value'   => set_value('league_id'),
+$leagueId = array(
+    'name'       => 'league_id',
+    'id'         => 'league_id',
+    'options'    => array('' => '--- Select ---') + $this->League_model->fetchForDropdown(),
+    'value'      => set_value('league_id', isset($leagueRegistration->league_id) ? $leagueRegistration->league_id : ''),
+    'attributes' => 'class="input-xlarge"',
 );
 
-$opposition_id = array(
-    'name'    => 'opposition_id',
-    'id'      => 'opposition_id',
-    'options' => array('' => '--- Select ---') + $this->Opposition_model->fetchForDropdown(),
-    'value'   => set_value('opposition_id'),
+$oppositionId = array(
+    'name'       => 'opposition_id',
+    'id'         => 'opposition_id',
+    'options'    => array('' => '--- Select ---') + $this->Opposition_model->fetchForDropdown(),
+    'value'      => set_value('opposition_id', isset($leagueRegistration->opposition_id) ? $leagueRegistration->opposition_id : ''),
+    'attributes' => 'class="input-xlarge"',
 );
 
+$submit = array(
+    'name'  => 'submit',
+    'class' => 'btn',
+    'value' => $submitButtonText,
+); ?>
+
+<?php
 echo form_open($this->uri->uri_string()); ?>
-<table>
-    <?php echo form_hidden('id', set_value('id', isset($leagueRegistration->id) ? $leagueRegistration->id : '')); ?>
-    <tr>
-        <td><?php echo form_label($this->lang->line('league_registration_league'), $league_id['name']); ?></td>
-        <td><?php echo form_dropdown($league_id['name'], $league_id['options'], set_value($league_id['name'], isset($leagueRegistration->league_id) ? $leagueRegistration->league_id : '')); ?></td>
-        <td class="error"><?php echo form_error($league_id['name']); ?><?php echo isset($errors[$league_id['name']]) ? $errors[$league_id['name']] : ''; ?></td>
-    </tr>
-    <tr>
-        <td><?php echo form_label($this->lang->line('league_registration_team'), $opposition_id['name']); ?></td>
-        <td><?php echo form_dropdown($opposition_id['name'], $opposition_id['options'], set_value($opposition_id['name'], isset($leagueRegistration->opposition_id) ? $leagueRegistration->opposition_id : '')); ?></td>
-        <td class="error"><?php echo form_error($opposition_id['name']); ?><?php echo isset($errors[$opposition_id['name']]) ? $errors[$opposition_id['name']] : ''; ?></td>
-    </tr>
-</table>
-<?php echo form_submit('submit', $submitButtonText); ?>
+    <?php echo form_hidden($id['name'], $id['value']); ?>
+    <fieldset>
+        <legend><?php echo $this->lang->line('league_registration_league_registration_details');?></legend>
+        <div class="control-group<?php echo form_error($leagueId['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('league_registration_league'), $leagueId['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($leagueId['name'], $leagueId['options'], $leagueId['value'], $leagueId['attributes']); ?>
+                <?php
+                if (form_error($leagueId['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($leagueId['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <div class="control-group<?php echo form_error($oppositionId['name']) ? ' error' : ''; ?>">
+            <?php echo form_label($this->lang->line('league_registration_team'), $oppositionId['id']); ?>
+            <div class="controls">
+                <?php echo form_dropdown($oppositionId['name'], $oppositionId['options'], $oppositionId['value'], $oppositionId['attributes']); ?>
+                <?php
+                if (form_error($oppositionId['name'])) { ?>
+                    <span class="help-inline"><?php echo form_error($oppositionId['name']); ?></span>
+                <?php
+                } ?>
+            </div>
+        </div>
+        <?php echo form_submit($submit); ?>
+    </fieldset>
 <?php echo form_close(); ?>
