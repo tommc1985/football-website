76a4f85c011d7bd5b3bea9a43b7720c4f5d46e4e
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-08 13:14:50 +0100

    Fix bug which was not showing date of match on preview

--- application/views/themes/default/match/match_details.php
+++ application/views/themes/default/match/match_details.php
@@ -1,14 +1,14 @@
 <h3><?php echo $this->lang->line('match_details'); ?></h3>
 
 <dl class="dl-horizontal">
+    <dt><?php echo $this->lang->line('match_date'); ?></dt>
+    <dd><time itemprop="startDate" datetime="<?php echo Utility_helper::formattedDate($match->date, "c"); ?>"><?php echo Utility_helper::longDateTime($match->date); ?></time></dd>
     <?php
     if (!$preview) { ?>
-        <dt><?php echo $this->lang->line('match_date'); ?></dt>
-        <dd><time itemprop="startDate" datetime="<?php echo Utility_helper::formattedDate($match->date, "c"); ?>"><?php echo Utility_helper::longDateTime($match->date); ?></time></dd>
+        <dt><?php echo $this->lang->line('match_score'); ?></dt>
+        <dd><?php echo Match_helper::longScore($match); ?></dd>
     <?php
     } ?>
-    <dt><?php echo $this->lang->line('match_score'); ?></dt>
-    <dd><?php echo Match_helper::longScore($match); ?></dd>
     <dt><?php echo $this->lang->line('match_venue'); ?></dt>
     <dd><span itemprop="name" itemscope itemtype="http://schema.org/SportsTeam"><?php echo Match_helper::longVenue($match) . ' ' . $this->lang->line('match_versus'); ?> <span itemprop="name"><?php echo Opposition_helper::name($match->opposition_id); ?></span></span></dd>
     <dt><?php echo $this->lang->line('match_competition'); ?></dt>
