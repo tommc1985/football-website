c68c102076acb3a147ad9a7b8ce3ef006f229bc8
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-09 11:23:06 +0100

    Basic Styling for League page

--- application/views/themes/default/league/view.php
+++ application/views/themes/default/league/view.php
@@ -8,18 +8,18 @@
                 <h3><?php echo $this->lang->line('league_league_table'); ?></h3>
                 <?php
                 if ($standings) { ?>
-                <table class="no-more-tables width-100-percent">
+                <table class="no-more-tables width-100-percent table table-striped table-condensed">
                     <thead>
                         <tr>
                             <td class="width-60-percent">&nbsp;</td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_p"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_w"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_d"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_l"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_f"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_a"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_gd"); ?></td>
-                            <td class="width-5-percent"><?php echo $this->lang->line("league_pts"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_p"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_w"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_d"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_l"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_f"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_a"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_gd"); ?></td>
+                            <td class="width-5-percent text-align-center"><?php echo $this->lang->line("league_pts"); ?></td>
                         </tr>
                     </thead>
                     <tbody>
@@ -27,14 +27,14 @@
                     foreach ($standings as $standing) { ?>
                         <tr itemscope itemtype="http://schema.org/SportsTeam">
                             <td itemprop="name" data-title="<?php echo $this->lang->line('league_team'); ?>"><?php echo Opposition_helper::name($standing->opposition_id); ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_played'); ?>"><?php echo $standing->played; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_won'); ?>"><?php echo $standing->won; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_drawn'); ?>"><?php echo $standing->drawn; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_lost'); ?>"><?php echo $standing->lost; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_for'); ?>"><?php echo $standing->gf; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_against'); ?>"><?php echo $standing->ga; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_goal_difference'); ?>"><?php echo $standing->gd; ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_points'); ?>"><?php echo $standing->points; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_played'); ?>"><?php echo $standing->played; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_won'); ?>"><?php echo $standing->won; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_drawn'); ?>"><?php echo $standing->drawn; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_lost'); ?>"><?php echo $standing->lost; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_for'); ?>"><?php echo $standing->gf; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_against'); ?>"><?php echo $standing->ga; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_goal_difference'); ?>"><?php echo $standing->gd; ?></td>
+                            <td class="text-align-center" data-title="<?php echo $this->lang->line('league_points'); ?>"><?php echo $standing->points; ?></td>
                         </tr>
                     <?php
                     } ?>
@@ -52,13 +52,13 @@
                 <h3><?php echo sprintf($this->lang->line("league_form_last_x_matches"), $formMatchCount); ?></h3>
                 <?php
                 if ($formTeams) { ?>
-                <table class="no-more-tables width-100-percent">
+                <table class="no-more-tables width-100-percent table table-striped table-condensed">
                     <tbody>
                     <?php
                     foreach ($formTeams as $opposition_id => $formTeam) { ?>
                         <tr itemscope itemtype="http://schema.org/SportsTeam">
                             <td itemprop="name" data-title="<?php echo $this->lang->line('league_team'); ?>"><?php echo Opposition_helper::name($standings[$opposition_id]->opposition_id); ?></td>
-                            <td data-title="<?php echo $this->lang->line('league_form'); ?>"><?php echo League_helper::formattedForm($standings[$opposition_id]->form, Configuration::get('form_match_count')); ?></td>
+                            <td data-title="<?php echo $this->lang->line('league_form'); ?>"><?php echo League_helper::formattedForm($standings[$opposition_id]->form, $formMatchCount); ?></td>
                         </tr>
                     <?php
                     } ?>
