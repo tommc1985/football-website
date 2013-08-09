d6c1f0fea4126e93de0ed66db0a33be115cf3d6a
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-10 00:56:48 +0100

    Add menu to League Statistics

--- application/views/themes/default/league-statistics/view.php
+++ application/views/themes/default/league-statistics/view.php
@@ -3,6 +3,17 @@
 
         <h2><?php echo $this->lang->line('league_statistics_title'); ?> - <?php echo League_helper::name($id); ?></h2>
 
+        <h3><?php echo $this->lang->line("league_statistics_statistics_menu"); ?></h3>
+        <ul class="nav nav-tabs nav-stacked">
+        <?php
+        foreach ($this->Cache_League_Statistics_model->methodMap as $statisticGroup => $method) {
+            foreach ($venues as $venue) { ?>
+            <li><a href="#<?php echo $statisticGroup; ?>"><?php echo $this->lang->line("league_statistics_{$statisticGroup}" . (strlen($venue) > 0 ? "_{$venue}" : '')); ?></a></li>
+        <?php
+            }
+        } ?>
+        </ul>
+
 <?php
 $i = 0;
 foreach ($this->Cache_League_Statistics_model->methodMap as $statisticGroup => $method) {
