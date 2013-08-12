064769b5e54c973595d49434c7565053316def69
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-12 15:34:05 +0100

    Moved date to just below title in News and Article views

--- application/views/themes/default/article/view.php
+++ application/views/themes/default/article/view.php
@@ -3,8 +3,8 @@
         <h2><?php echo $this->lang->line('article_title'); ?></h2>
 
         <h3 itemprop="headline"><?php echo $article->title; ?></h3>
-        <p itemprop="articleBody"><?php echo nl2br($article->content); ?></p>
         <p class="muted"><?php echo $this->lang->line('article_published_on'); ?> <time itemprop="datePublished" datetime="<?php echo Utility_helper::formattedDate($article->publish_date, "c"); ?>"><?php echo Utility_helper::longDateTime($article->publish_date); ?></time></p>
+        <p itemprop="articleBody"><?php echo nl2br($article->content); ?></p>
 
     </div>
 </div>
--- application/views/themes/default/news/view.php
+++ application/views/themes/default/news/view.php
@@ -3,7 +3,7 @@
         <h2><?php echo $this->lang->line('news_title'); ?></h2>
 
         <h3 itemprop="headline"><?php echo $article->title; ?></h3>
-        <p itemprop="articleBody"><?php echo nl2br($article->content); ?></p>
         <p class="muted"><?php echo $this->lang->line('news_published_on'); ?> <time itemprop="datePublished" datetime="<?php echo Utility_helper::formattedDate($article->publish_date, "c"); ?>"><?php echo Utility_helper::longDateTime($article->publish_date); ?></time></p>
+        <p itemprop="articleBody"><?php echo nl2br($article->content); ?></p>
     </div>
 </div>
