8efcb32fd110ab5c4831a1cbc6de792ac6f6cfe5
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-14 18:57:20 +0100

    Changed credit text in footer

--- application/views/themes/default/footer.php
+++ application/views/themes/default/footer.php
@@ -8,7 +8,7 @@
 
     <footer class="footer">
         <div class="container">
-            <p>Coded by <a href="http://twitter.com/tommc1985" target="_blank">@tommc1985</a>.</p>
+            <p>Built by <a href="http://twitter.com/tommc1985" target="_blank">@tommc1985</a>.</p>
             <p>Codebase available to download for free from <a href="https://github.com/tommc1985/football-website" target="_blank">GitHub</a> and licensed under <a href="http://www.apache.org/licenses/LICENSE-2.0" target="_blank">Apache License v2.0</a>.</p>
             <p>Built with a combination of <a href="http://codeigniter.com" target="_blank">CodeIgniter</a>, <a href="http://getbootstrap.com/" target="_blank">Bootstrap 2.3.2</a>, <a href="http://fortawesome.github.io/Font-Awesome/icons" target="_blank">Font Awesome</a> &amp; <a href="http://www.chartjs.org/" target="_blank">ChartJS</a></p>
         </div>
@@ -19,8 +19,8 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script type="text/javascript">
     var baseURL = '<?php echo site_url(); ?>';
+
     </script>
     <?php echo Assets::js(); ?>
-
 </body>
 </html>
