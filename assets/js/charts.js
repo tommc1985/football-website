fdd18482148e66d32a9ee57324d8dfcd7db855d9
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-14 14:13:37 +0100

    Add automatic generation of charts based on parsed data

---
+++ assets/js/charts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+    for (i in charts) {
+        generateChart(charts[i]);
+    }
+});
+
+function generateChart(chart) {
+    var ctx = $("#" + chart.id).get(0).getContext("2d");
+
+    switch (chart.type) {
+        case 'line':
+            new Chart(ctx).Line(chart.data, chart.options);
+            break;
+        case 'radar':
+            new Chart(ctx).Radar(chart.data, chart.options);
+            break;
+        case 'polar':
+            new Chart(ctx).PolarArea(chart.data, chart.options);
+            break;
+        case 'pie':
+            new Chart(ctx).Pie(chart.data, chart.options);
+            break;
+        case 'doughnut':
+            new Chart(ctx).Doughnut(chart.data, chart.options);
+            break;
+        default :
+            new Chart(ctx).Bar(chart.data, chart.options);
+    }
+}
