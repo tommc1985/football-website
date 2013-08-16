9e6d562861f5d6befcde7b932258aded09931efa
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-16 15:31:25 +0100

    Tidied Fantasy Football language file

--- application/language/english/fantasy_football_lang.php
+++ application/language/english/fantasy_football_lang.php
@@ -10,76 +10,76 @@ $lang['fantasy_football_view_frontend_meta_description_all_time']          = 'Fa
 $lang['fantasy_football_view_frontend_meta_title_all_time_and_type']       = 'All Time Fantasy Football (%3$s matches only)';
 $lang['fantasy_football_view_frontend_meta_description_all_time_and_type'] = 'Fantasy Football Statistics for %1$s for all time (%3$s matches only), with features including leaderboards for points earned in a particular position (ordered by total points earned or average points earned) and best line up in a specified formation based on Total Points, Average Points or Appearances.';
 
-$lang['fantasy_football_title']                     = 'Fantasy Football';
+$lang['fantasy_football_title']                                            = 'Fantasy Football';
 
-$lang['fantasy_football_explanation']                     = 'Just for a bit of fun, Fantasy Football involves players receiving points for making different contributions in every competitive match, whether it be an appearance, a goal, a clean sheet on an assist. Players can also lose points for things such as being issued red or yellow cards.';
+$lang['fantasy_football_explanation']                                      = 'Just for a bit of fun, Fantasy Football involves players receiving points for making different contributions in every competitive match, whether it be an appearance, a goal, a clean sheet on an assist. Players can also lose points for things such as being issued red or yellow cards.';
 
-$lang['fantasy_football_leaderboard_explanation']                     = 'View the current Fantasy Football leaderboard, ordered by the total number of points each player has accumulated, or the average number of points they earn per game. You can also use the position dropdown to see exactly how players peform in a particular position.';
+$lang['fantasy_football_leaderboard_explanation']                          = 'View the current Fantasy Football leaderboard, ordered by the total number of points each player has accumulated, or the average number of points they earn per game. You can also use the position dropdown to see exactly how players peform in a particular position.';
 
-$lang['fantasy_football_best_lineup_explanation']                     = 'An algorithm is used to determine the best lineup for the specified formation, based on a number of factors including the position(s) a player would prefer to appear in, which players would be preferred in a specific position, and a third measurement to be determined by you, selectable from the dropdown below. This can vary from the total or average number of points a player has earned in a specific (or similar) position or the number of appearances in a specific (or similar) position.';
+$lang['fantasy_football_best_lineup_explanation']                          = 'An algorithm is used to determine the best lineup for the specified formation, based on a number of factors including the position(s) a player would prefer to appear in, which players would be preferred in a specific position, and a third measurement to be determined by you, selectable from the dropdown below. This can vary from the total or average number of points a player has earned in a specific (or similar) position or the number of appearances in a specific (or similar) position.';
 
-$lang['fantasy_football_scoring_system_explanation']                     = 'The table below describes how Fantasy Football points are earned by each player.';
+$lang['fantasy_football_scoring_system_explanation']                       = 'The table below describes how Fantasy Football points are earned by each player.';
 
-$lang['fantasy_football_matches']                   = 'Matches';
+$lang['fantasy_football_matches']                                          = 'Matches';
 
-$lang['fantasy_football_leaderboard']               = 'Leaderboard';
-$lang['fantasy_football_best_lineup']               = 'Best Lineup';
-$lang['fantasy_football_scoring_system']            = 'Scoring System';
-$lang['fantasy_football_lineup']                    = 'Lineup';
-
-
-$lang['fantasy_football_competition_type']          = 'Competition Type';
-
-$lang['fantasy_football_show']                      = 'Show';
-
-$lang['fantasy_football_player']                    = 'Player';
-$lang['fantasy_football_appearances']               = 'Appearances';
-$lang['fantasy_football_points']                    = 'Points';
-$lang['fantasy_football_average']                   = 'Average';
-$lang['fantasy_football_position']                  = 'Position';
-
-$lang['fantasy_football_order_by']                  = 'Order By';
-
-$lang['fantasy_football_all']                       = 'All';
-$lang['fantasy_football_refresh']                   = 'Refresh';
-$lang['fantasy_football_measurement']               = 'Measurement';
-$lang['fantasy_football_formation']                 = 'Formation';
-
-$lang['fantasy_football_total_points']              = 'Total Points';
-$lang['fantasy_football_average_points']            = 'Average Points';
-$lang['fantasy_football_appearances']               = 'Appearances';
-
-$lang['fantasy_football_criteria']                  = 'Criteria';
+$lang['fantasy_football_leaderboard']                                      = 'Leaderboard';
+$lang['fantasy_football_best_lineup']                                      = 'Best Lineup';
+$lang['fantasy_football_scoring_system']                                   = 'Scoring System';
+$lang['fantasy_football_lineup']                                           = 'Lineup';
 
 
-$lang['fantasy_football_pt']                        = 'pt';
-$lang['fantasy_football_pts']                       = 'pts';
-$lang['fantasy_football_app']                       = 'app';
-$lang['fantasy_football_apps']                      = 'apps';
+$lang['fantasy_football_competition_type']                                 = 'Competition Type';
+
+$lang['fantasy_football_show']                                             = 'Show';
+
+$lang['fantasy_football_player']                                           = 'Player';
+$lang['fantasy_football_appearances']                                      = 'Appearances';
+$lang['fantasy_football_points']                                           = 'Points';
+$lang['fantasy_football_average']                                          = 'Average';
+$lang['fantasy_football_position']                                         = 'Position';
+
+$lang['fantasy_football_order_by']                                         = 'Order By';
+
+$lang['fantasy_football_all']                                              = 'All';
+$lang['fantasy_football_refresh']                                          = 'Refresh';
+$lang['fantasy_football_measurement']                                      = 'Measurement';
+$lang['fantasy_football_formation']                                        = 'Formation';
+
+$lang['fantasy_football_total_points']                                     = 'Total Points';
+$lang['fantasy_football_average_points']                                   = 'Average Points';
+$lang['fantasy_football_appearances']                                      = 'Appearances';
+
+$lang['fantasy_football_criteria']                                         = 'Criteria';
 
 
-
-$lang['fantasy_football_starting_appearance']       = 'Starting Appearance in a Match';
-$lang['fantasy_football_substitute_appearance']     = 'Substitute Appearance in a Match';
-$lang['fantasy_football_clean_sheet_by_goalkeeper'] = 'Clean Sheet By Goalkeeper';
-$lang['fantasy_football_clean_sheet_by_defender']   = 'Clean Sheet By Defender';
-$lang['fantasy_football_clean_sheet_by_midfielder'] = 'Clean Sheet By Midfielder';
-$lang['fantasy_football_assist_by_goalkeeper']      = 'Assist By Goalkeeper';
-$lang['fantasy_football_assist_by_defender']        = 'Assist By Defender';
-$lang['fantasy_football_assist_by_midfielder']      = 'Assist By Midfielder';
-$lang['fantasy_football_assist_by_striker']         = 'Assist By Striker';
-$lang['fantasy_football_goal_by_goalkeeper']        = 'Goal By Goalkeeper';
-$lang['fantasy_football_goal_by_defender']          = 'Goal By Defender';
-$lang['fantasy_football_goal_by_midfielder']        = 'Goal By Midfielder';
-$lang['fantasy_football_goal_by_striker']           = 'Goal By Striker';
-$lang['fantasy_football_man_of_the_match']          = 'Man of the Match';
-$lang['fantasy_football_yellow_card']               = 'Yellow Card';
-$lang['fantasy_football_red_card']                  = 'Red Card';
+$lang['fantasy_football_pt']                                               = 'pt';
+$lang['fantasy_football_pts']                                              = 'pts';
+$lang['fantasy_football_app']                                              = 'app';
+$lang['fantasy_football_apps']                                             = 'apps';
 
 
 
-$lang['fantasy_football_no_data']                   = 'No data for the specified season/competition';
-$lang['fantasy_football_formation_not_exist']       = 'Formation does not exist';
+$lang['fantasy_football_starting_appearance']                              = 'Starting Appearance in a Match';
+$lang['fantasy_football_substitute_appearance']                            = 'Substitute Appearance in a Match';
+$lang['fantasy_football_clean_sheet_by_goalkeeper']                        = 'Clean Sheet By Goalkeeper';
+$lang['fantasy_football_clean_sheet_by_defender']                          = 'Clean Sheet By Defender';
+$lang['fantasy_football_clean_sheet_by_midfielder']                        = 'Clean Sheet By Midfielder';
+$lang['fantasy_football_assist_by_goalkeeper']                             = 'Assist By Goalkeeper';
+$lang['fantasy_football_assist_by_defender']                               = 'Assist By Defender';
+$lang['fantasy_football_assist_by_midfielder']                             = 'Assist By Midfielder';
+$lang['fantasy_football_assist_by_striker']                                = 'Assist By Striker';
+$lang['fantasy_football_goal_by_goalkeeper']                               = 'Goal By Goalkeeper';
+$lang['fantasy_football_goal_by_defender']                                 = 'Goal By Defender';
+$lang['fantasy_football_goal_by_midfielder']                               = 'Goal By Midfielder';
+$lang['fantasy_football_goal_by_striker']                                  = 'Goal By Striker';
+$lang['fantasy_football_man_of_the_match']                                 = 'Man of the Match';
+$lang['fantasy_football_yellow_card']                                      = 'Yellow Card';
+$lang['fantasy_football_red_card']                                         = 'Red Card';
+
+
+
+$lang['fantasy_football_no_data']                                          = 'No data for the specified season/competition';
+$lang['fantasy_football_formation_not_exist']                              = 'Formation does not exist';
 
 /* End of file fantasy_football.php */
 /* Location: ./application/language/english/fantasy_football.php */
