ac367432830bcbf2b3d38d8a746fb9dd516719f6
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-16 11:42:12 +0100

    Removed "Page Rendered..." text in footer

--- application/views/themes/default/footer.php
+++ application/views/themes/default/footer.php
@@ -1,8 +1,8 @@
-    <div class="row-fluid">
+    <?php /*<div class="row-fluid">
         <div class="span12">
             Page rendered in <strong>{elapsed_time}</strong> seconds
         </div>
-    </div>
+    </div>*/ ?>
 </div>
 
 
