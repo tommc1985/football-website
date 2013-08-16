d814a987dbce7603c17ecda64a07d782aad43765
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-16 14:59:02 +0100

    Increased Clean Sheet Fantasy Football points

--- config_values.php
+++ config_values.php
@@ -8,9 +8,9 @@ $configValues['usual_match_ko_time']              = '10:30';
 
 $configValues['starting_appearance_points']       = '2';
 $configValues['substitute_appearance_points']     = '1';
-$configValues['clean_sheet_by_goalkeeper_points'] = '5';
-$configValues['clean_sheet_by_defender_points']   = '3';
-$configValues['clean_sheet_by_midfielder_points'] = '1';
+$configValues['clean_sheet_by_goalkeeper_points'] = '9';
+$configValues['clean_sheet_by_defender_points']   = '7';
+$configValues['clean_sheet_by_midfielder_points'] = '3';
 $configValues['assist_by_goalkeeper_points']      = '9';
 $configValues['assist_by_defender_points']        = '7';
 $configValues['assist_by_midfielder_points']      = '3';
