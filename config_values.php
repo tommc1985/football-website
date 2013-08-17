da8b55d3347152479579a4953e4d77223bf23fa3
Thomas McGregor <leegleeders@yahoo.co.uk>
2013-08-17 12:25:13 +0100

    Updated Fantasy Football scoring values to make it fairer toward midfielders, defenders and goalkeepers

--- config_values.php
+++ config_values.php
@@ -6,12 +6,12 @@ $configValues['earliest_season']                  = '1997';
 
 $configValues['usual_match_ko_time']              = '10:30';
 
-$configValues['starting_appearance_points']       = '2';
-$configValues['substitute_appearance_points']     = '1';
-$configValues['clean_sheet_by_goalkeeper_points'] = '9';
-$configValues['clean_sheet_by_defender_points']   = '7';
-$configValues['clean_sheet_by_midfielder_points'] = '3';
-$configValues['assist_by_goalkeeper_points']      = '9';
+$configValues['starting_appearance_points']       = '5';
+$configValues['substitute_appearance_points']     = '2';
+$configValues['clean_sheet_by_goalkeeper_points'] = '15';
+$configValues['clean_sheet_by_defender_points']   = '10';
+$configValues['clean_sheet_by_midfielder_points'] = '5';
+$configValues['assist_by_goalkeeper_points']      = '10';
 $configValues['assist_by_defender_points']        = '7';
 $configValues['assist_by_midfielder_points']      = '3';
 $configValues['assist_by_striker_points']         = '5';
